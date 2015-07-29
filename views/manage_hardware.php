5c0e464e021db1f885fa27953cd2f33a8bf9b3f2
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-29 21:34:10 +0530

    manage_hardware.php initial commit

---
+++ views/manage_hardware.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DCIMStack</title>
+    <?php include 'libraries/css.php'; ?>
+  </head>
+
+  <body>
+
+    <?php include 'libraries/header.php'; ?>
+
+    <div class="container-fluid">
+      <div class="row">
+        <?php include 'libraries/sidebar.php'; ?>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Manage Rackspace</h1>
+        </div>
+      </div>
+    </div>
+        
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <?php include 'libraries/js.php'; ?>
+  </body>
+</html>
