1fa7008ab402b850665dff481cd5f2b056a465bc
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-29 19:27:59 +0530

    Add alerts library

---
+++ libraries/alerts.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_SESSION['success'])) {
+	$success = $_SESSION['success'];
+	echo "<div class='alert alert-success alert-dismissible' role='alert'>";
+        echo "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
+        echo "$success";
+    echo "</div>";
+    unset($_SESSION['success']);
+    unset($success);
+}
+if(isset($_SESSION['error'])) {
+	$error = $_SESSION['error'];
+	echo "<div class='alert alert-warning alert-dismissible' role='alert'>";
+        echo "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
+        echo "$error";
+    echo "</div>";
+    unset($_SESSION['error']);
+    unset($error);
+}
+?>
