44224cf910115a43e023c2202ed61661052c3219
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-29 17:13:31 +0530

    Add in CSS libs

---
+++ libraries/css.php
@@ -0,0 +1,2 @@
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
