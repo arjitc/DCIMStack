bf36f093b027e1dae48d6c14963a4a0c63e2c368
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-29 20:24:37 +0530

    Don't hardset DB name and added add_event function

--- views/add_rackspace_db.php
+++ views/add_rackspace_db.php
@@ -1,12 +1,17 @@
 <?php
 include 'config/db.php';
+include 'libraries/events.php';
 $rack_name    = mysqli_real_escape_string($conn, $_POST['rack_name']);
 $rack_size    = mysqli_real_escape_string($conn, $_POST['rack_size']);
 $rack_city    = mysqli_real_escape_string($conn, $_POST['rack_city']);
 $rack_country = mysqli_real_escape_string($conn, $_POST['rack_country']);
 if(isset($rack_name, $rack_size, $rack_city, $rack_country)) {
-	$sql = "INSERT INTO `dcimstack`.`rackspace` (`id`, `rack_name`, `rack_size`, `rackspace_used`, `rack_city`, `rack_country`) VALUES (NULL, '$rack_name', '$rack_size', '0', '$rack_city', '$rack_country');";
+	$sql = "INSERT INTO `rackspace` (`id`, `rack_name`, `rack_size`, `rack_used`, `rack_city`, `rack_country`) VALUES (NULL, '$rack_name', '$rack_size', '0', '$rack_city', '$rack_country');";
 	if ($conn->query($sql) === TRUE) {
+		$event_type = "New Rackspace";
+		$event_message = "New rackspace $rack_name added";
+		$event_status = "Complete";
+		add_event($event_type, $event_message, $event_status);
     	$_SESSION['success'] = "Success, Rackspace added into DCIMStack";
     	header('Location: add_rackspace.php');
 	} else {
