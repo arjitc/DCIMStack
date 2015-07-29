48e7e6ff05c5d660b133d862fc6c345ce78ff5c6
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-29 17:46:29 +0530

    Update CSS libs

--- libraries/css.php
+++ libraries/css.php
@@ -1,2 +1,3 @@
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/dashboard.css" rel="stylesheet">
