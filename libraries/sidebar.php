54f46ecdd6731663dd016e3f6369123cd47d6117
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-29 18:26:03 +0530

    Updated sidebar.php to set <li> to active when on that page

--- libraries/sidebar.php
+++ libraries/sidebar.php
@@ -1,11 +1,28 @@
+<?php include_once 'general.php'; ?>
 <div class="col-sm-3 col-md-2 sidebar">
   <ul class="nav nav-sidebar">
-    <li class="active"><a href="#">Overview</a></li>
+    <?php 
+    if(get_filename_from_url()=="index.php") {
+      echo "<li class='active'><a href='#'>Overview</a></li>";
+    } else {
+      echo "<li><a href='index.php'>Overview</a></li>";
+    }
+    ?>
   </ul>
   <ul class="nav nav-sidebar">
     <li class="dropdown-header">Rackspace</li>
-    <li><a href="">Manage Rackspace</a></li>
-    <li><a href="">Add Rackspace</a></li>
+    <?php 
+    if(get_filename_from_url()=="manage_rackspace.php") {
+      echo "<li class='active'><a href='manage_rackspace.php'>Manage Rackspace</a></li>";
+    } else {
+      echo "<li><a href='manage_rackspace.php'>Manage Rackspace</a></li>";
+    }
+    if(get_filename_from_url()=="add_rackspace.php") {
+      echo "<li class='active'><a href='add_rackspace.php'>Add Rackspace</a></li>";
+    } else {
+      echo "<li><a href='add_rackspace.php'>Add Rackspace</a></li>";
+    }
+    ?>
     <hr>
     <li class="dropdown-header">Hardware</li>
     <li><a href="">Manage Hardware</a></li>
