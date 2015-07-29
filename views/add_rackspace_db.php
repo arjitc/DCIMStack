b9588a1df6d409c44d346c72fbfa9ee231f7d0d7
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-29 19:29:20 +0530

    Finished add_rackspace_db.php

--- views/add_rackspace_db.php
+++ views/add_rackspace_db.php
@@ -1,6 +1,18 @@
 <?php
-echo $_POST['rack_name'];
-echo $_POST['rack_size'];
-echo $_POST['rack_city'];
-echo $_POST['rack_country'];
+include 'config/db.php';
+$rack_name    = mysqli_real_escape_string($conn, $_POST['rack_name']);
+$rack_size    = mysqli_real_escape_string($conn, $_POST['rack_size']);
+$rack_city    = mysqli_real_escape_string($conn, $_POST['rack_city']);
+$rack_country = mysqli_real_escape_string($conn, $_POST['rack_country']);
+if(isset($rack_name, $rack_size, $rack_city, $rack_country)) {
+	$sql = "INSERT INTO `dcimstack`.`rackspace` (`id`, `rack_name`, `rack_size`, `rackspace_used`, `rack_city`, `rack_country`) VALUES (NULL, '$rack_name', '$rack_size', '0', '$rack_city', '$rack_country');";
+	if ($conn->query($sql) === TRUE) {
+    	$_SESSION['success'] = "Success, Rackspace added into DCIMStack";
+    	header('Location: add_rackspace.php');
+	} else {
+		$_SESSION['error'] = "Error, Rackspace not added into DCIMStack";
+		header('Location: add_rackspace.php');
+	}
+	$conn->close();
+}
 ?>
