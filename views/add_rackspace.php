7ea9476106bbf91bdf6b135b542ecaf6fc896712
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-29 18:41:40 +0530

    Add rackspace UI + backend initial commit

--- views/add_rackspace.php
+++ views/add_rackspace.php
@@ -17,7 +17,45 @@
         <?php include 'libraries/sidebar.php'; ?>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Add Rackspace</h1>
-          
+          <span class="text-muted">New Rackspace can be added into DCIMStack using the form below.</span>
+          <hr>
+          <form method="post" action="add_rackspace_db.php" class="form-horizontal">
+            <div class="form-group">
+              <label for="inputEmail3" class="col-sm-2 control-label">Name</label>
+              <div class="col-sm-10">
+                <input type="text" name="rack_name" class="form-control" placeholder="Let's name your rack" required>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputPassword3" class="col-sm-2 control-label">Rack Size</label>
+              <div class="col-sm-10">
+                <select class="form-control" name="rack_size">
+                  <?php
+                  for($i=1;$i<=42;$i++) {
+                    echo "<option value='$i'>".$i."U</option>";
+                  }
+                  ?>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputPassword3" class="col-sm-2 control-label">City</label>
+              <div class="col-sm-10">
+                <input type="text" name="rack_city" class="form-control" placeholder="Which City is your rack in?" required>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputPassword3" class="col-sm-2 control-label">Country</label>
+              <div class="col-sm-10">
+                <input type="text" name="rack_country" class="form-control" placeholder="Which Country is your rack in?" required>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10">
+                <center><button type="submit" class="btn btn-primary">Add Rack</button></center>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
---
+++ views/add_rackspace_db.php
@@ -0,0 +1,6 @@
+<?php
+echo $_POST['rack_name'];
+echo $_POST['rack_size'];
+echo $_POST['rack_city'];
+echo $_POST['rack_country'];
+?>
