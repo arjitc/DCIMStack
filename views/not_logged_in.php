7f3ef844e0d36b6e75215e1ab5b7c3d60b2750e2
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-29 17:28:02 +0530

    Login page UI updates

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -1,30 +1,70 @@
-<?php
-// show potential errors / feedback (from login object)
-if (isset($login)) {
-    if ($login->errors) {
-        foreach ($login->errors as $error) {
-            echo $error;
-        }
-    }
-    if ($login->messages) {
-        foreach ($login->messages as $message) {
-            echo $message;
-        }
-    }
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-<!-- login form box -->
-<form method="post" action="index.php" name="loginform">
+    <title>Starter Template for Bootstrap</title>
 
-    <label for="login_input_username">Username</label>
-    <input id="login_input_username" class="login_input" type="text" name="user_name" required />
+    <!-- Bootstrap core CSS -->
+    <?php include 'libraries/css.php'; ?>
+  </head>
 
-    <label for="login_input_password">Password</label>
-    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
+  <body>
+    <div style='padding-bottom:40px;'></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <center><h1><i class="fa fa-database"></i> StackDCIM</h1></center>
+                        <?php
+                        // show potential errors / feedback (from login object)
+                        if (isset($login)) {
+                            if ($login->errors) {
+                                foreach ($login->errors as $error) {
+                                    echo "<div class='alert alert-warning alert-dismissible' role='alert'>";
+                                        echo "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
+                                        echo "<center>$error</center>";
+                                    echo "</div>";
+                                }
+                            }
+                            if ($login->messages) {
+                                foreach ($login->messages as $message) {
+                                    echo "<div class='alert alert-success alert-dismissible' role='alert'>";
+                                        echo "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
+                                        echo "<center>$message</center>";
+                                    echo "</div>";;
+                                }
+                            }
+                        }
+                        ?>
+                        <!-- login form box -->
+                        <form method="post" action="index.php" id="loginform">
+                            <input id="login_input_username" class="form-control" type="text" name="user_name" required>
+                            <div style='padding-bottom:5px;'></div>
+                            <input id="login_input_password" class="form-control" type="password" name="user_password" autocomplete="off" required>
+                        </form>
+                    </div>
+                    <div class="panel-footer">
+                        <center><input type="submit" class="btn btn-primary" form="loginform" name="login" value="Log in"></center>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div><!-- /.container -->
 
-    <input type="submit"  name="login" value="Log in" />
 
-</form>
-
-<a href="register.php">Register new account</a>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <?php include 'libraries/js.php'; ?>
+  </body>
+</html>
