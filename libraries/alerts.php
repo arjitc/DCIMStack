e1c2affac1ab77202f38ec6e0536ba1b773c3671
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-02 17:44:33 +0530

    Add icons to success / error messages

--- libraries/alerts.php
+++ libraries/alerts.php
@@ -3,7 +3,7 @@ if(isset($_SESSION['success'])) {
 	$success = $_SESSION['success'];
 	echo "<div class='alert alert-success alert-dismissible' role='alert'>";
         echo "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
-        echo "$success";
+        echo "<i class='fa fa-check'></i> $success";
     echo "</div>";
     unset($_SESSION['success']);
     unset($success);
@@ -12,7 +12,7 @@ if(isset($_SESSION['error'])) {
 	$error = $_SESSION['error'];
 	echo "<div class='alert alert-warning alert-dismissible' role='alert'>";
         echo "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
-        echo "$error";
+        echo "<i class='fa fa-times'></i> $error";
     echo "</div>";
     unset($_SESSION['error']);
     unset($error);
