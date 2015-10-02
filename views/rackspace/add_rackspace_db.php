736ab7c0d8a09df0c97644f379b7e90ebe42a770
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-02 17:47:43 +0530

    Add check for each element, if missing then return back with specific error

--- views/rackspace/add_rackspace_db.php
+++ views/rackspace/add_rackspace_db.php
@@ -5,8 +5,11 @@ $rack_name    = mysqli_real_escape_string($conn, $_POST['rack_name']);
 $rack_size    = mysqli_real_escape_string($conn, $_POST['rack_size']);
 $rack_city    = mysqli_real_escape_string($conn, $_POST['rack_city']);
 $rack_country = mysqli_real_escape_string($conn, $_POST['rack_country']);
-if(isset($rack_name, $rack_size, $rack_city, $rack_country)) {
-	$sql = "INSERT INTO `rackspace` (`id`, `rack_name`, `rack_size`, `rack_used`, `rack_city`, `rack_country`) VALUES (NULL, '$rack_name', '$rack_size', '0', '$rack_city', '$rack_country');";
+$rack_power   = mysqli_real_escape_string($conn, $_POST['rack_power']);
+$rack_voltage = mysqli_real_escape_string($conn, $_POST['rack_voltage']);
+if(isset($rack_name, $rack_size, $rack_city, $rack_country, $rack_power, $rack_voltage)) {
+	$sql = "INSERT INTO `dcimstack`.`rackspace` (`id`, `rack_name`, `rack_size`, `rack_used`, `rack_power`, `rack_voltage`, `rack_city`, `rack_country`) 
+			VALUES (NULL, '$rack_name', '$rack_size', '$rack_used', '$rack_power', '$rack_voltage', '$rack_city', '$rack_country');";
 	if ($conn->query($sql) === TRUE) {
 		$event_type = "New Rackspace";
 		$event_message = "New rackspace $rack_name added";
@@ -19,5 +22,29 @@ if(isset($rack_name, $rack_size, $rack_city, $rack_country)) {
 		header('Location: add_rackspace.php');
 	}
 	$conn->close();
+} 
+if(empty($rack_name)) {
+	$_SESSION['error'] = "Error, Rack Name not set.";
+	header('Location: add_rackspace.php');
+}
+if(empty($rack_size)) {
+	$_SESSION['error'] = "Error, Rack Size not set.";
+	header('Location: add_rackspace.php');
+}
+if(empty($rack_city)) {
+	$_SESSION['error'] = "Error, Rack City not set.";
+	header('Location: add_rackspace.php');
+}
+if(empty($rack_country)) {
+	$_SESSION['error'] = "Error, Rack Country not set.";
+	header('Location: add_rackspace.php');
+}
+if(empty($rack_power)) {
+	$_SESSION['error'] = "Error, Rack Power not set.";
+	header('Location: add_rackspace.php');
+}
+if(empty($rack_voltage)) {
+	$_SESSION['error'] = "Error, Rack Voltage not set.";
+	header('Location: add_rackspace.php');
 }
 ?>
