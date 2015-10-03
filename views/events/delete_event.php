6bae065a7e42c2c7ed79075e9bf762de38dd1747
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-03 17:50:13 +0530

    Update delete event

--- views/events/delete_event.php
+++ views/events/delete_event.php
@@ -1,4 +1,19 @@
 <?php
-
-
+include 'config/db.php';
+if($_SESSION['token']==$_GET['token']) {
+	$token = $_GET['token'];
+	$id = $_GET['id'];
+	$sql = "DELETE FROM `events` WHERE `id`='$id'";
+	if ($conn->query($sql) === TRUE) {
+    	$_SESSION['success'] = "Success, Event deleted.";
+    	header('Location: index.php');
+	} else {
+		$_SESSION['error'] = "Error, Event not deleted.";
+	    header('Location: index.php');
+	}
+	unset($_SESSION['token']);
+} else {
+	echo "Token mismatch";
+	unset($_SESSION['token']);
+}
 ?>
