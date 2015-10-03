dff4f53153fdc766a33d332e178d41113040ecfb
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-03 17:51:30 +0530

    (int) $id

--- views/events/delete_event.php
+++ views/events/delete_event.php
@@ -1,7 +1,7 @@
 <?php
 include 'config/db.php';
 if($_SESSION['token']==$_GET['token']) {
-	$id = mysqli_real_escape_string($conn, $_GET['id']);
+	$id = mysqli_real_escape_string($conn, (int)$_GET['id']);
 	$sql = "DELETE FROM `events` WHERE `id`='$id'";
 	if ($conn->query($sql) === TRUE) {
     	$_SESSION['success'] = "Success, Event deleted.";
