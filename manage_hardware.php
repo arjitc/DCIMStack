b58f9187ba50d310e2a54f006853cf540addb8d6
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-03 17:32:11 +0530

    update manage_hardware.php path

--- manage_hardware.php
+++ manage_hardware.php
@@ -34,7 +34,7 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
-    include("views/manage_hardware.php");
+    include("views/hardware/manage_hardware.php");
 
 } else {
     // the user is not logged in. you can do whatever you want here.
