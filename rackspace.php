250de10794ecaf9260d09aa3eb9e39eb3d6d19bc
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-04 03:25:52 +0530

    Move rackspace.php to it's own folder

--- rackspace.php
+++ rackspace.php
@@ -34,7 +34,7 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
-    include("views/rackspace.php");
+    include("views/rackspace/rackspace.php");
 
 } else {
     // the user is not logged in. you can do whatever you want here.
