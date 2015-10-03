a16429fa4e27e255655f5273ab532f8de59c799c
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-03 17:31:59 +0530

    Add CSRF to events

--- libraries/events.php
+++ libraries/events.php
@@ -8,18 +8,22 @@ function add_event($event_type, $event_message, $event_status) {
 	$conn->query($sql);
 }
 function list_events_table() {
+	$token = md5(uniqid(rand(), TRUE));
+    $_SESSION['token'] = $token;
 	include realpath(dirname(__FILE__)).'/../config/db.php';
 	$sql = "SELECT * FROM `events`";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 	    // output data of each row
 	    while($row = $result->fetch_assoc()) {
+	    	$id = $row["id"];
 	    	echo "<tr>";
 	            echo "<td>".$row["id"]."</td>";
 	            echo "<td>".$row["event_type"]."</td>";
 	            echo "<td>".$row["event_message"]."</td>";
 	            echo "<td>".$row["event_status"]."</td>";
 	            echo "<td>".$row["event_timestamp"]."</td>";
+	            echo "<td><a href='delete_event.php?id=$id&token=$token' class='confirmation'><i class='fa fa-times'></i></a></td>";
             echo "</tr>";
 	    }
 	} else {
