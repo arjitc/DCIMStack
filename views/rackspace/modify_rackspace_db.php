a6be16330a90cedfc3da1a5067eaa89e854a3fe8
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-04 21:01:53 +0530

    Modify_rackspace_db init

---
+++ views/rackspace/modify_rackspace_db.php
@@ -0,0 +1,50 @@
+<?php
+include 'config/db.php';
+include 'libraries/events.php';
+$rack_name    = mysqli_real_escape_string($conn, $_POST['rack_name']);
+$rack_size    = mysqli_real_escape_string($conn, $_POST['rack_size']);
+$rack_city    = mysqli_real_escape_string($conn, $_POST['rack_city']);
+$rack_country = mysqli_real_escape_string($conn, $_POST['rack_country']);
+$rack_power   = mysqli_real_escape_string($conn, $_POST['rack_power']);
+$rack_voltage = mysqli_real_escape_string($conn, $_POST['rack_voltage']);
+if(isset($rack_name, $rack_size, $rack_city, $rack_country, $rack_power, $rack_voltage)) {
+	//$sql = "INSERT INTO `dcimstack`.`rackspace` (`id`, `rack_name`, `rack_size`, `rack_size_used`, `rack_power`, `rack_voltage`, `rack_city`, `rack_country`) 
+	//		VALUES (NULL, '$rack_name', '$rack_size', '$rack_used', '$rack_power', '$rack_voltage', '$rack_city', '$rack_country');";
+	if ($conn->query($sql) === TRUE) {
+		$event_type = "Rackspace Modified";
+		$event_message = "Rackspace $rack_name was updated";
+		$event_status = "Complete";
+		add_event($event_type, $event_message, $event_status);
+    	$_SESSION['success'] = "Success, Rackspace modified.";
+    	header('Location: manage_rackspace.php');
+	} else {
+		$_SESSION['error'] = "Error, Rackspace not modified.";
+		header('Location: manage_rackspace.php');
+	}
+	$conn->close();
+} 
+if(empty($rack_name)) {
+	$_SESSION['error'] = "Error, Rack Name not set.";
+	header('Location: manage_rackspace.php');
+}
+if(empty($rack_size)) {
+	$_SESSION['error'] = "Error, Rack Size not set.";
+	header('Location: manage_rackspace.php');
+}
+if(empty($rack_city)) {
+	$_SESSION['error'] = "Error, Rack City not set.";
+	header('Location: manage_rackspace.php');
+}
+if(empty($rack_country)) {
+	$_SESSION['error'] = "Error, Rack Country not set.";
+	header('Location: manage_rackspace.php');
+}
+if(empty($rack_power)) {
+	$_SESSION['error'] = "Error, Rack Power not set.";
+	header('Location: manage_rackspace.php');
+}
+if(empty($rack_voltage)) {
+	$_SESSION['error'] = "Error, Rack Voltage not set.";
+	header('Location: manage_rackspace.php');
+}
+?>
