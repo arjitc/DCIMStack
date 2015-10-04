991d053f995091fd4b09f1d7fa6bd2b670aacb2d
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-04 17:08:59 +0530

    Fix indents

--- views/rackspace/modify_rackspace.php
+++ views/rackspace/modify_rackspace.php
@@ -27,61 +27,61 @@ if ($result->num_rows > 0) {
         <div class="modal-body">
             <form method="post" id="modify_rack_form" action="add_rackspace_db.php" class="form-horizontal">
             <div class="form-group">
-              <label for="rack_name" class="col-sm-2 control-label">Name</label>
-              <div class="col-sm-10">
-                <input type="text" name="rack_name" class="form-control" value='<?php echo $rack_name; ?>' placeholder="Let's name your rack" required>
-              </div>
+                <label for="rack_name" class="col-sm-2 control-label">Name</label>
+                <div class="col-sm-10">
+                    <input type="text" name="rack_name" class="form-control" value='<?php echo $rack_name; ?>' placeholder="Let's name your rack" required>
+                </div>
             </div>
             <div class="form-group">
-              <label for="rack_size" class="col-sm-2 control-label">Rack Size</label>
-              <div class="col-sm-10">
-                <select class="form-control" name="rack_size">
-                    <?php
-                        echo "<option value='$rack_size'>".$rack_size."U</option>";
-                        for($i=1;$i<=48;$i++) {
-                        if($rack_size!=$i) {
-                            echo "<option value='$i'>".$i."U</option>";
-                        }
-                    }
-                    ?>
-                </select>
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="rack_size" class="col-sm-2 control-label">Power</label>
-              <div class="col-sm-10">
-                <select class="form-control" name="rack_power">
-                    <?php
-                        echo "<option value='$rack_power'>".$rack_power."A</option>";
-                            for($i=1;$i<=80;$i++) {
-                                if($rack_power!=$i) {
-                                    echo "<option value='$i'>".$i."A</option>";
-                                }
+                <label for="rack_size" class="col-sm-2 control-label">Rack Size</label>
+                <div class="col-sm-10">
+                    <select class="form-control" name="rack_size">
+                        <?php
+                            echo "<option value='$rack_size'>".$rack_size."U</option>";
+                            for($i=1;$i<=48;$i++) {
+                            if($rack_size!=$i) {
+                                echo "<option value='$i'>".$i."U</option>";
                             }
-                    ?>
-                </select>
-              </div>
+                        }
+                        ?>
+                    </select>
+                </div>
             </div>
             <div class="form-group">
-              <label for="rack_size" class="col-sm-2 control-label">Voltage</label>
-              <div class="col-sm-10">
-                <select class="form-control" name="rack_voltage">
-                  <option value="120V">120V</option>
-                  <option value="208V">208V</option>
-                </select>
-              </div>
+                <label for="rack_size" class="col-sm-2 control-label">Power</label>
+                <div class="col-sm-10">
+                    <select class="form-control" name="rack_power">
+                        <?php
+                            echo "<option value='$rack_power'>".$rack_power."A</option>";
+                                for($i=1;$i<=80;$i++) {
+                                    if($rack_power!=$i) {
+                                        echo "<option value='$i'>".$i."A</option>";
+                                    }
+                                }
+                        ?>
+                    </select>
+                </div>
             </div>
             <div class="form-group">
-              <label for="rack_city" class="col-sm-2 control-label">City</label>
-              <div class="col-sm-10">
-                <input type="text" name="rack_city" class="form-control" value='<?php echo $rack_city; ?>' placeholder="Which City is your rack in?" required>
-              </div>
+                <label for="rack_size" class="col-sm-2 control-label">Voltage</label>
+                <div class="col-sm-10">
+                    <select class="form-control" name="rack_voltage">
+                      <option value="120V">120V</option>
+                      <option value="208V">208V</option>
+                    </select>
+                </div>
             </div>
             <div class="form-group">
-              <label for="rack_country" class="col-sm-2 control-label">Country</label>
-              <div class="col-sm-10">
-                <input type="text" name="rack_country" class="form-control" value='<?php echo $rack_country; ?>' placeholder="Which Country is your rack in?" required>
-              </div>
+                <label for="rack_city" class="col-sm-2 control-label">City</label>
+                <div class="col-sm-10">
+                    <input type="text" name="rack_city" class="form-control" value='<?php echo $rack_city; ?>' placeholder="Which City is your rack in?" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="rack_country" class="col-sm-2 control-label">Country</label>
+                <div class="col-sm-10">
+                    <input type="text" name="rack_country" class="form-control" value='<?php echo $rack_country; ?>' placeholder="Which Country is your rack in?" required>
+                </div>
             </div>
         </div>
         <div class="modal-footer">
