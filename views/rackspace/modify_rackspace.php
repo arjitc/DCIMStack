d578415d0a4f9e4a2a5413757b7ec48670516771
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-04 17:05:50 +0530

    Update form and it's elements values

--- views/rackspace/modify_rackspace.php
+++ views/rackspace/modify_rackspace.php
@@ -1,7 +1,22 @@
 <?php
 include 'libraries/general.php';
-$id = $_GET['id'];
+include 'config/db.php';
+$id = mysqli_real_escape_string($conn, $_GET['id']);
 check_if_rack_exists($id);
+$sql = "SELECT * FROM `rackspace` WHERE `id`='$id'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $id             = $row["id"];
+        $rack_name      = $row["rack_name"];
+        $rack_size      = $row["rack_size"];
+        $rack_size_used = $row["rack_size_used"]."U";
+        $rack_city      = $row["rack_city"];
+        $rack_country   = $row["rack_country"];
+        $rack_voltage   = $row["rack_voltage"]."V";
+        $rack_power     = $row["rack_power"];
+    }
+}
 ?>
 <div class="modal-dialog">
     <div class="modal-content">
@@ -10,11 +25,69 @@ check_if_rack_exists($id);
             <h4 class="modal-title">Edit <?php echo get_rack_name($id); ?></h4>
         </div>
         <div class="modal-body">
+            <form method="post" id="modify_rack_form" action="add_rackspace_db.php" class="form-horizontal">
+            <div class="form-group">
+              <label for="rack_name" class="col-sm-2 control-label">Name</label>
+              <div class="col-sm-10">
+                <input type="text" name="rack_name" class="form-control" value='<?php echo $rack_name; ?>' placeholder="Let's name your rack" required>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="rack_size" class="col-sm-2 control-label">Rack Size</label>
+              <div class="col-sm-10">
+                <select class="form-control" name="rack_size">
+                    <?php
+                        echo "<option value='$rack_size'>".$rack_size."U</option>";
+                        for($i=1;$i<=48;$i++) {
+                        if($rack_size!=$i) {
+                            echo "<option value='$i'>".$i."U</option>";
+                        }
+                    }
+                    ?>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="rack_size" class="col-sm-2 control-label">Power</label>
+              <div class="col-sm-10">
+                <select class="form-control" name="rack_power">
+                    <?php
+                        echo "<option value='$rack_power'>".$rack_power."A</option>";
+                            for($i=1;$i<=80;$i++) {
+                                if($rack_power!=$i) {
+                                    echo "<option value='$i'>".$i."A</option>";
+                                }
+                            }
+                    ?>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="rack_size" class="col-sm-2 control-label">Voltage</label>
+              <div class="col-sm-10">
+                <select class="form-control" name="rack_voltage">
+                  <option value="120V">120V</option>
+                  <option value="208V">208V</option>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="rack_city" class="col-sm-2 control-label">City</label>
+              <div class="col-sm-10">
+                <input type="text" name="rack_city" class="form-control" value='<?php echo $rack_city; ?>' placeholder="Which City is your rack in?" required>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="rack_country" class="col-sm-2 control-label">Country</label>
+              <div class="col-sm-10">
+                <input type="text" name="rack_country" class="form-control" value='<?php echo $rack_country; ?>' placeholder="Which Country is your rack in?" required>
+              </div>
+            </div>
         </div>
         <div class="modal-footer">
             <a href="#" class="btn btn-white" data-dismiss="modal">Close</a>
-            <a href="#" class="btn btn-primary">Button</a>
-            <a href="#" class="btn btn-primary">Another button...</a>
+            <button type="submit" form="modify_rack_form" class="btn btn-primary">Add Rack</button>
+        </form>
         </div>
     </div><!-- /.modal-content -->
 </div><!-- /.modal-dialog -->
