865b62cd7aa296da1743128ceace859f7c0ca72f
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-05 15:51:30 +0530

    unset token before redirect and set token mismatch error return

--- views/events/delete_event.php
+++ views/events/delete_event.php
@@ -5,14 +5,16 @@ if($_SESSION['token']==$_GET['token']) {
 	$sql = "DELETE FROM `events` WHERE `id`='$id'";
 	if ($conn->query($sql) === TRUE) {
     	$_SESSION['success'] = "Success, Event deleted.";
+    	unset($_SESSION['token']);
     	header('Location: index.php');
 	} else {
 		$_SESSION['error'] = "Error, Event not deleted.";
+		unset($_SESSION['token']);
 	    header('Location: index.php');
 	}
-	unset($_SESSION['token']);
 } else {
-	echo "Token mismatch";
+	$_SESSION['error'] = "Error, Token mismatch.";
 	unset($_SESSION['token']);
+	header('Location: index.php');
 }
 ?>
