33097e140e93397943f4dda999cc01315180e10b
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-12 02:47:01 +0530

    Switch to device_* instead of server_*

--- views/rackspace/servers/servers.php
+++ views/rackspace/servers/servers.php
@@ -1,16 +1,7 @@
 <?php
-include_once 'libraries/general.php';
-include_once 'config/db.php';
-include_once 'libraries/cpu_count.php';
-//error_reporting(-1);
-//ini_set('display_errors', 'On');
-check_if_rack_exists($_GET['rackid']); //this checks if the rack exists, if the rack does not exist it redirects the user back to index.php
-?>
-
-<?php
 include realpath(dirname(__FILE__)).'/../config/db.php';
 $rackid  = mysqli_real_escape_string($conn, (int)$_GET['rackid']);
-$sql = "SELECT * FROM `servers` WHERE `rackid`='$rackid'";
+$sql = "SELECT * FROM `devices` WHERE `device_type`='server' AND `rackid`='$rackid'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
@@ -29,21 +20,21 @@ if ($result->num_rows > 0) {
             </thead>
           <tbody>";
   	    while($row = $result->fetch_assoc()) {
-  	    	$id = $row["server_id"];
+  	    	$id = $row["device_id"];
   	    	echo "<tr>";
-                echo "<td>".$row["server_rack_position"]."</td>";
-  	            echo "<td>".$row["server_brand"]."</td>";
-  	            echo "<td>".$row["server_label"]."</td>";
-  	            echo "<td>".$row["server_ram_total"]."</td>";
-                echo "<td>".server_cpu_count($row["server_cpu"], $row["server_cpu_count"])."</td>";
-  	            echo "<td>".$row["server_size"]."U</td>";
-  	            echo "<td>".$row["server_serial"]."</td>";
+                echo "<td>".$row["device_rack_position"]."</td>";
+  	            echo "<td>".vendor_logo($row["device_brand"])."</td>";
+  	            echo "<td>".$row["device_label"]."</td>";
+  	            echo "<td>".$row["device_ram_total"]."</td>";
+                echo "<td>".device_cpu_count($row["device_cpu"], $row["device_cpu_count"])."</td>";
+  	            echo "<td>".$row["device_size"]."U</td>";
+  	            echo "<td>".$row["device_serial"]."</td>";
               echo "</tr>";
   	    }
   	    echo "</tbody>
               </table>
             </div>";
   	} else {
-  	    echo "<center>No servers found</center>";
+  	    echo "<center>No Servers Found</center>";
   	}     
-  	?>
+?>
