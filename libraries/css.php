d1a2f155607e0a3f585cd98df1d1dadf34d7c274
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-12 03:26:42 +0530

    Change bootstrap template

--- libraries/css.php
+++ libraries/css.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/spacelab/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="assets/dashboard.css" rel="stylesheet">
