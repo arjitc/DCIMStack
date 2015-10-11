f464bc7a1ff7881de9b879741c95d30b65f6c15f
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-12 02:40:38 +0530

    Add AJAX modal

--- libraries/js.php
+++ libraries/js.php
@@ -6,3 +6,18 @@
         return confirm('Are you sure?');
     });
 </script>
+
+<script type="text/javascript">
+    $('[data-toggle="ajaxModal"]').on('click',
+              function(e) {
+                $('#ajaxModal').remove();
+                e.preventDefault();
+                var $this = $(this)
+                  , $remote = $this.data('remote') || $this.attr('href')
+                  , $modal = $('<div class="modal" id="ajaxModal"><div class="modal-body"></div></div>');
+                $('body').append($modal);
+                $modal.modal({backdrop: 'static', keyboard: false});
+                $modal.load($remote);
+              }
+            );
+</script>
