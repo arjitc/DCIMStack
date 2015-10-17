e6f8a8debdb4def3b2c5902de4398d3753edfe4b
Arjit Chaudhary <arjit.c@gmail.com>
2015-10-17 19:59:43 +0530

    New Alert Warning for static error messages

--- libraries/alerts.php
+++ libraries/alerts.php
@@ -10,11 +10,17 @@ if(isset($_SESSION['success'])) {
 }
 if(isset($_SESSION['error'])) {
 	$error = $_SESSION['error'];
-	echo "<div class='alert alert-warning alert-dismissible' role='alert'>";
+	echo "<div class='alert alert-danger alert-dismissible' role='alert'>";
         echo "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
         echo "<i class='fa fa-times'></i> $error";
     echo "</div>";
     unset($_SESSION['error']);
     unset($error);
+}
+function alert_warning($alert_message) {
+    echo "<div class='alert alert-warning alert-dismissible' role='alert'>";
+        echo "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
+        echo "<i class='fa fa-times'></i> $alert_message";
+    echo "</div>";
 }
 ?>
