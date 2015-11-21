9fa39e42c8d449d70ff910ba025724fcd296c786
Arjit Chaudhary <arjit.c@gmail.com>
2015-11-21 15:57:04 +0530

    Add datatables to tables

--- libraries/js.php
+++ libraries/js.php
@@ -1,6 +1,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
+<script src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.10/js/dataTables.bootstrap.min.js"></script>
 <script type="text/javascript">
     $('.confirmation').on('click', function () {
         return confirm('Are you sure?');
@@ -20,4 +21,10 @@
                 $modal.load($remote);
               }
             );
+</script>
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#search_table').DataTable();
+} );
 </script>
