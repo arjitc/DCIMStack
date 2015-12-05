4626b19f88413f713669c8c733a6c8a81dc50b86
Arjit Chaudhary <arjit.c@gmail.com>
2015-12-05 23:58:03 +0530

    Add device_dop and device_warranty

--- views/hardware/add_device_db.php
+++ views/hardware/add_device_db.php
@@ -19,38 +19,42 @@ $device_cpu = $_POST['device_cpu'];
 $device_rack_position = $_POST['device_rack_position'];
 $device_size = $_POST['device_size'];
 $device_notes = $_POST['device_notes'];
+$device_dop = $_POST['device_dop'];
+$device_warranty = $_POST['device_warranty'];
 if(empty($device_size)) $device_size=0;
-$sql = "INSERT INTO `dcimstack`.`devices` 
-		(`device_id`, `rackid`, `device_type`, `device_label`, `device_brand`, `device_serial`, `device_mac`, `device_ram_total`, `device_capacity`, `device_cpu_count`, `device_power_usage`, `device_power_feed1`, `device_power_feed2`, `device_cpu`, `device_rack_position`, `device_size`, `device_notes`) 
-		VALUES 
-		(NULL, 
-			'$device_location', 
-			'$device_type', 
-			'$device_label', 
-			'$device_brand', 
-			'$device_serial', 
-			'$device_mac', 
-			'$device_ram_total', 
-			'$device_capacity', 
-			'$device_cpu_count', 
-			'$device_power_usage', 
-			'$device_power_feed1', 
-			'$device_power_feed2', 
-			'$device_cpu', 
-			'$device_rack_position', 
-			'$device_size', 
-			'$device_notes');";
+$sql = "INSERT INTO `dcimstack`.`devices`
+(`device_id`, `rackid`, `device_type`, `device_label`, `device_brand`, `device_serial`, `device_mac`, `device_ram_total`, `device_capacity`, `device_cpu_count`, `device_power_usage`, `device_power_feed1`, `device_power_feed2`, `device_cpu`, `device_rack_position`, `device_size`, `device_warranty`,`device_dop`,`device_notes`)
+VALUES
+(NULL,
+	'$device_location',
+	'$device_type',
+	'$device_label',
+	'$device_brand',
+	'$device_serial',
+	'$device_mac',
+	'$device_ram_total',
+	'$device_capacity',
+	'$device_cpu_count',
+	'$device_power_usage',
+	'$device_power_feed1',
+	'$device_power_feed2',
+	'$device_cpu',
+	'$device_rack_position',
+	'$device_size',
+	'$device_dop',
+	'$device_warranty',
+	'$device_notes');";
 
-if ($conn->query($sql) === TRUE) {
-	$rack_name = get_rack_name($device_location);
-    //echo "New record created successfully";
-    $event_type = "New $device_type added";
-	$event_message = "A new $device_type was added to $rack_name";
-	$event_status = "Complete";
-	add_event($event_type, $event_message, $event_status);
-    $conn->close();
-    header("Location: $page_referrer");
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-?>
+	if ($conn->query($sql) === TRUE) {
+		$rack_name = get_rack_name($device_location);
+		//echo "New record created successfully";
+		$event_type = "New $device_type added";
+		$event_message = "A new $device_type was added to $rack_name";
+		$event_status = "Complete";
+		add_event($event_type, $event_message, $event_status);
+		$conn->close();
+		header("Location: $page_referrer");
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	?>
