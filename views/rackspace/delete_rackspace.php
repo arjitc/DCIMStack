c85b75d2c6b609ad9f57758e462519191c8bfd0b
Arjit Chaudhary <arjit.c@gmail.com>
2016-02-12 19:00:02 +0530

    Fix code style

--- views/rackspace/delete_rackspace.php
+++ views/rackspace/delete_rackspace.php
@@ -1,19 +1,19 @@
 <?php
 include 'config/db.php';
-if($_SESSION['token']==$_GET['token']) {
-  $rackid = mysqli_real_escape_string($conn, (int)$_GET['rackid']);
-  $sql = "DELETE FROM `rackspace` WHERE `rackid`='$rackid'";
-  if ($conn->query($sql) === TRUE) {
-      $_SESSION['success'] = "Success, Rackspace deleted.";
-      unset($_SESSION['token']);
-      header('Location: manage_rackspace.php');
-  } else {
-    $_SESSION['error'] = "Error, Rackspace not deleted.";
-    unset($_SESSION['token']);
-      header('Location: manage_rackspace.php');
-  }
+if($_SESSION['token'] == $_GET['token']) {
+	$rackid = mysqli_real_escape_string($conn, (int)$_GET['rackid']);
+	$sql = "DELETE FROM `rackspace` WHERE `rackid`='$rackid'";
+	if ($conn->query($sql) === TRUE) {
+    	$_SESSION['success'] = "Success, Rackspace deleted.";
+    	unset($_SESSION['token']);
+    	header('Location: manage_rackspace.php');
+	} else {
+		$_SESSION['error'] = "Error, Rackspace not deleted.";
+		unset($_SESSION['token']);
+	    header('Location: manage_rackspace.php');
+	}
 } else {
-  echo "Token mismatch";
-  unset($_SESSION['token']);
+	echo "Token mismatch";
+	unset($_SESSION['token']);
 }
 ?>
