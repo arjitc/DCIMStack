bd39380a0e36a5195cdb67098ff3805f8bc982ba
Arjit Chaudhary <arjit.c@gmail.com>
2016-02-16 22:19:50 +0530

    port_status(), port_label() and print_ports() functions

--- views/rackspace/networking/networking_functions.php
+++ views/rackspace/networking/networking_functions.php
@@ -1,24 +1,58 @@
 <?php
-function print_ports($physical_label, $port_count) {
-	$count = 1;
-		echo "<h4>$physical_label</h4>";
-		echo "<hr>";
-		for($i=0; $i<4; $i++) {
-			echo "<div class='row'>";
-			for($j=0; $j<$port_count; $j++) {
-				echo "<div class='col-md-2'>";
-				echo "<div class='panel panel-success'>
-					  <div class='panel-heading'>
-					    <h3 class='panel-title'><center><small>Port</small><br> $count</center></h3>
-					  </div>
-					  <div class='panel-body'>
-					    Panel content
-					  </div>
-					</div>";
-				echo "</div>";
-				$count++;
-			}
-			echo "</div>";
-		}
+function port_status($port_number, $device_id) {
+	include 'config/db.php';
+	$sql = "SELECT * FROM `networking` WHERE `device_id`='$device_id' AND `port_number`='$port_number'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        return $row["port_status"];
+	    }
+	} else {
+	    return 0;
 	}
+}
+function port_label($port_number, $device_id) {
+	include 'config/db.php';
+	$sql = "SELECT * FROM `networking` WHERE `device_id`='$device_id' AND `port_number`='$port_number'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	    	return $row["port_label"];
+	    }
+	} else {
+	    return "Port";
+	}
+}
+function print_ports($physical_label, $port_count, $device_id) {
+	$port_number = 1;
+	echo "<h4>$physical_label</h4>";
+	echo "<hr>";
+	for($i=0; $i<4; $i++) {
+		echo "<div class='row'>";
+		for($j=0; $j<$port_count; $j++) {
+			echo "<div class='col-md-2'>";
+			if(port_status($port_number,$device_id) == 0) {
+				echo "<div class='panel panel-warning'>";
+			} else {
+				echo "<div class='panel panel-success'>";
+			}
+				$port_label = port_label($port_number, $device_id);
+				echo"<div class='panel-heading'>
+				    <h3 class='panel-title'><center><small>$port_label</small><br> $port_number</center></h3>
+				  </div>
+				  <div class='panel-body'>";
+				  echo "<center>";
+				  echo "<img src='assets/img/arrow_up.png'> 5 MB/s";
+				  echo "<img src='assets/img/arrow_down.png'> 4 MB/s";
+				  echo "</center>";
+				echo"</div>
+				</div>";
+			echo "</div>";
+			$port_number++;
+		}
+		echo "</div>";
+	}
+}
 ?>
