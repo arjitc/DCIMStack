5f6ffdb64d9002ebb8f61c822904d7176919a919
Arjit Chaudhary <arjit.c@gmail.com>
2016-02-16 23:28:57 +0530

    Ability to add in LibreNMS API settings

--- views/settings/settings.php
+++ views/settings/settings.php
@@ -9,6 +9,26 @@ if ($result->num_rows > 0) {
 } else {
     //echo "0 results";
 }
+$sql = "SELECT * FROM `settings` WHERE `setting`='librenms_api_key'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        $librenms_api_key = $row["value"];
+    }
+} else {
+    //echo "0 results";
+}
+$sql = "SELECT * FROM `settings` WHERE `setting`='librenms_api_endpoint'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        $librenms_api_endpoint = $row["value"];
+    }
+} else {
+    //echo "0 results";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -45,6 +65,39 @@ if ($result->num_rows > 0) {
             </div>
             <div class="panel-footer">
               <center><input type="submit" form="aftership_api_key" value="Update" class="btn btn-primary"></center>
+            </div>
+            </form>
+          </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title"><img src='assets/img/lorry.png'> LibreNMS API Key</h3>
+            </div>
+            <div class="panel-body">
+              <p>Current LibreNMS API Key: <?php echo $librenms_api_key; ?></p>
+              <form action="settings_db.php" id="librenms_api_key" method="post">
+                <input type="hidden" name="token" value="<?php echo $token; ?>">
+                <input type="hidden" name="setting" value="librenms_api_key">
+                <input type="text" name="value" placeholder="LibreNMS API Key" class="form-control" required>
+            </div>
+            <div class="panel-footer">
+              <center><input type="submit" form="librenms_api_key" value="Update" class="btn btn-primary"></center>
+            </div>
+            </form>
+          </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title"><img src='assets/img/lorry.png'> LibreNMS API Endpoint</h3>
+            </div>
+            <div class="panel-body">
+              <p>Example: http://librenms.domain.com/api/v0/devices/</p>
+              <p>Current LibreNMS API Endpoint: <?php echo $librenms_api_endpoint; ?></p>
+              <form action="settings_db.php" id="librenms_api_endpoint" method="post">
+                <input type="hidden" name="token" value="<?php echo $token; ?>">
+                <input type="hidden" name="setting" value="librenms_api_endpoint">
+                <input type="text" name="value" placeholder="LibreNMS API Endpoint" class="form-control" required>
+            </div>
+            <div class="panel-footer">
+              <center><input type="submit" form="librenms_api_endpoint" value="Update" class="btn btn-primary"></center>
             </div>
             </form>
           </div>
