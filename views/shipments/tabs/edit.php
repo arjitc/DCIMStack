5806a9bc7c197dfee3dd2815308ad2c27fed8658
Arjit Chaudhary <arjit.c@gmail.com>
2016-02-19 19:19:42 +0530

    Ability to edit/update a shipment

--- views/shipments/tabs/edit.php
+++ views/shipments/tabs/edit.php
@@ -1,5 +1,6 @@
             <br>
-            <form action="edit_device.php" id="edit_shipment" method="post">
+            <form action="update_shipment_db.php" id="edit_shipment" method="post">
+                <input type="hidden" name="shipment_id" class="form-control" value="<?php echo $shipment_id; ?>" required>
                 <label>Shipment Tracking ID</label>
                 <input type="text" name="shipment_tracking_id" class="form-control" value="<?php echo $shipment_tracking_id; ?>" required>
                 <hr>
--- views/shipments/update_shipment_db.php
+++ views/shipments/update_shipment_db.php
@@ -1,12 +1,15 @@
 <?php
 include 'config/db.php';
-$tracking_id = mysqli_real_escape_string($conn, $_POST['tracking_id']);
-$shipping_courier = mysqli_real_escape_string($conn, $_POST['shipping_courier']);
-$delivery_eta = mysqli_real_escape_string($conn, $_POST['delivery_eta']);
-if(isset($_GET['delivery_status'])) {
+$tracking_id = mysqli_real_escape_string($conn, $_POST['shipment_tracking_id']);
+$shipment_id =  mysqli_real_escape_string($conn, $_POST['shipment_id']);
+if(isset($tracking_id)) {
     $delivery_status = mysqli_real_escape_string($conn, $_GET['delivery_status']);
+    $sql = "UPDATE `shipments` SET `shipment_tracking_id`='$tracking_id' WHERE `id`='$shipment_id'";
+    $conn->query($sql);
+    $_SESSION['success'] = "Success, Shipment updated.";
+    header('Location: shipments.php');
 } else {
-    $delivery_status = mysqli_real_escape_string($conn, $_POST['delivery_status']);
+  $_SESSION['error'] = "Error, Shipment not updated.";
+  header('Location: shipments.php');
 }
-
 ?>
