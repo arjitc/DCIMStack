487d91239fe621c2f9d96214f689ab8990574f45
Arjit Chaudhary <arjit.c@gmail.com>
2016-02-19 21:11:35 +0530

    Check for shipment_id as well

--- views/shipments/update_shipment_db.php
+++ views/shipments/update_shipment_db.php
@@ -2,7 +2,7 @@
 include 'config/db.php';
 $tracking_id = mysqli_real_escape_string($conn, $_POST['shipment_tracking_id']);
 $shipment_id =  mysqli_real_escape_string($conn, $_POST['shipment_id']);
-if(isset($tracking_id)) {
+if(isset($tracking_id, $shipment_id)) {
     $delivery_status = mysqli_real_escape_string($conn, $_GET['delivery_status']);
     $sql = "UPDATE `shipments` SET `shipment_tracking_id`='$tracking_id' WHERE `id`='$shipment_id'";
     $conn->query($sql);
