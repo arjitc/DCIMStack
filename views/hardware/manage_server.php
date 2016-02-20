e7683397488f8c624259641a6d362869ec7cf357
Arjit Chaudhary <arjit.c@gmail.com>
2016-02-20 17:37:30 +0530

    Manage server UI testing

--- views/hardware/manage_server.php
+++ views/hardware/manage_server.php
@@ -4,6 +4,7 @@ if (!ctype_digit($_GET['device_id'])) {
 } else {
     $_SESSION['referrer'] = "servers.php"; //manually set it here.
     include 'config/db.php';
+    include 'libraries/general.php';
     $device_id = mysqli_real_escape_string($conn, $_GET['device_id']);
     $sql = "SELECT * FROM `devices` WHERE `device_id`='$device_id'";
     $result = $conn->query($sql);
@@ -25,7 +26,55 @@ if (!ctype_digit($_GET['device_id'])) {
             <h4 class="modal-title"><img src='assets/img/drive_go.png'> Manage server - <?php echo $device_label; ?></h4>
         </div>
         <div class="modal-body">
-
+            <form action="add_device_db.php" id="add_hdds" method="post">
+                    <input type="hidden" class="form-control" name="device_type" value="server">
+                    <label>Server CPU</label>
+                    <input type="text" class="form-control" name="device_cpu">
+                    <label>CPU Count</label>
+                    <select class="form-control" name="device_cpu_count">
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                    </select>
+                    <label>RAM</label>
+                    <div class="input-group">
+                      <input type="text" class="form-control" name="device_ram_total">
+                      <div class="input-group-addon">GB</div>
+                    </div>
+                    <label>Server Size</label>
+                    <select class="form-control" name="device_size">
+                      <option value="1U">1U</option>
+                      <option value="2U">2U</option>
+                      <option value="3U">3U</option>
+                      <option value="4U">4U</option>
+                    </select>
+                    <label>Server Vendor</label>
+                    <select class="form-control" name="device_brand">
+                        <option value="<?php echo $device_brand; ?>"><?php echo $device_brand; ?></option>
+                        <option value="HP">HP</option>
+                        <option value="Dell">Dell</option>
+                        <option value="Supermicro">Supermicro</option>
+                        <option value="IBM">IBM</option>
+                    </select>
+                    <label>Device Location</label>
+                    <?php
+                    include 'config/db.php';
+                    $sql = "SELECT * FROM `rackspace`";
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                      echo "<select class='form-control' name='device_location'>";
+                        // output data of each row
+                        while ($row = $result->fetch_assoc()) {
+                          $rackid = $row["rackid"];
+                          echo "<option value='$rackid'>".get_rack_name($rackid)."</option>";
+                        }
+                        echo "</select>";
+                      } else {
+                        echo "0 results";
+                      }
+                      ?>
+            </form>
         </div>
         <div class="modal-footer">
             <div class="pull-left">
