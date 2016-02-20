f1b7afcb801eb124b59e9956b2407e52b1063012
Arjit Chaudhary <arjit.c@gmail.com>
2016-02-20 17:37:50 +0530

    Change to Tab UI for shipment management

--- views/shipments/shipment_manage.php
+++ views/shipments/shipment_manage.php
@@ -4,6 +4,7 @@ if (!ctype_digit($_GET['shipment_id'])) {
 } else {
     $_SESSION['referrer'] = "shipments.php"; //manually set it here.
     include 'config/db.php';
+    include 'libraries/general.php';
     $shipment_id = mysqli_real_escape_string($conn, $_GET['shipment_id']);
     $sql = "SELECT * FROM `shipments` WHERE `id`='$shipment_id'";
     $result = $conn->query($sql);
@@ -26,24 +27,24 @@ if (!ctype_digit($_GET['shipment_id'])) {
         </div>
         <div class="modal-body">
             <div id="content">
-        <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
-            <li class="active"><a href="#edit" data-toggle="tab"><img src="assets/img/pencil.png"> Edit</a></li>
-            <li><a href="#notes" data-toggle="tab"><img src="assets/img/note.png"> Notes</a></li>
-            <li><a href="#move_to_hardware" data-toggle="tab"><img src="assets/img/keyboard.png"> Move to Hardware</a></li>
-        </ul>
-        <div id="my-tab-content" class="tab-content">
-            <div class="tab-pane active" id="edit">
-                <?php include 'tabs/edit.php'; ?>
-            </div>
-            <div class="tab-pane" id="move_to_hardware">
-                <?php include 'tabs/move_to_hardware.php'; ?>
-            </div>
-            <div class="tab-pane" id="notes">
-                <?php include 'tabs/notes.php'; ?>
+                <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
+                    <li class="active"><a href="#edit" data-toggle="tab"><img src="assets/img/pencil.png"> Edit</a></li>
+                    <li><a href="#notes" data-toggle="tab"><img src="assets/img/note.png"> Notes</a></li>
+                    <li><a href="#move_to_hardware" data-toggle="tab"><img src="assets/img/keyboard.png"> Move to Hardware</a></li>
+                </ul>
+                <div id="my-tab-content" class="tab-content">
+                    <div class="tab-pane active" id="edit">
+                        <?php include 'tabs/edit.php'; ?>
+                    </div>
+                    <div class="tab-pane" id="move_to_hardware">
+                        <?php include 'tabs/move_to_hardware.php'; ?>
+                    </div>
+                    <div class="tab-pane" id="notes">
+                        <?php include 'tabs/notes.php'; ?>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
         <div class="modal-footer">
             <div class="pull-left">
             <a href="delete_shipment.php?shipment_id=<?php echo $shipment_id; ?>" class="btn btn-danger confirmation">Delete Shipment</a>
