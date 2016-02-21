ddcb65a7ee668cc4e3f03cc92260f9ee1b779ab6
Arjit Chaudhary <arjit.c@gmail.com>
2016-02-21 17:50:39 +0530

    Cleanup

--- libraries/general.php
+++ libraries/general.php
@@ -1,132 +1,134 @@
 <?php
 function get_filename_from_url() {
-  $filename = basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']);
-  return $filename;
+	$filename = basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']);
+	return $filename;
 }
 function get_rack_name($id) {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $id  = mysqli_real_escape_string($conn, $id);
-  $sql = "SELECT * FROM `rackspace` WHERE `rackid`='$id'";
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-          $rack_name = $row["rack_name"];
-      }
-  } else {
-      $rack_name = "None";
-  }
-  return $rack_name;
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$id  = mysqli_real_escape_string($conn, $id);
+	$sql = "SELECT * FROM `rackspace` WHERE `rackid`='$id'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$rack_name = $row["rack_name"];
+		}
+	} else {
+		$rack_name = "None";
+	}
+	return $rack_name;
 }
 function check_if_rack_exists($id) {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $id  = mysqli_real_escape_string($conn, $id);
-  $sql = "SELECT * FROM `rackspace` WHERE `rackid`='$id'";
-  $result = $conn->query($sql);
-  if ($result->num_rows == 0) {
-    header('Location: index.php');
-  }
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$id  = mysqli_real_escape_string($conn, $id);
+	$sql = "SELECT * FROM `rackspace` WHERE `rackid`='$id'";
+	$result = $conn->query($sql);
+	if ($result->num_rows == 0) {
+		header('Location: index.php');
+	}
 }
 function check_if_feed_exists($feedid) {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $feedid  = mysqli_real_escape_string($conn, $feedid);
-  $sql = "SELECT * FROM `power_feeds` WHERE `feed_id`='$feedid'";
-  $result = $conn->query($sql);
-  if ($result->num_rows == 0) {
-    header('Location: index.php');
-  }
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$feedid  = mysqli_real_escape_string($conn, $feedid);
+	$sql = "SELECT * FROM `power_feeds` WHERE `feed_id`='$feedid'";
+	$result = $conn->query($sql);
+	if ($result->num_rows == 0) {
+		header('Location: index.php');
+	}
 }
 function get_device_label_from_id($device_id) {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $id  = mysqli_real_escape_string($conn, $id);
-  $sql = "SELECT * FROM `devices` WHERE `device_id`='$device_id'";
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-          $device_label = $row["device_label"];
-      }
-  } else {
-      $device_label = "None";
-  }
-  return $device_label;
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$id  = mysqli_real_escape_string($conn, $id);
+	$sql = "SELECT * FROM `devices` WHERE `device_id`='$device_id'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			return $row["device_label"];
+		}
+	} else {
+		return "None";
+	}
+	$conn->close();
 }
 function get_tracking_from_id($shipping_id) {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $shipping_id  = mysqli_real_escape_string($conn, $shipping_id);
-  $sql = "SELECT * FROM `shipments` WHERE `id`='$shipping_id'";
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-          $tracking_id = $row["tracking_id"];
-      }
-  } else {
-      $tracking_id = "None";
-  }
-  return $tracking_id;
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$shipping_id  = mysqli_real_escape_string($conn, $shipping_id);
+	$sql = "SELECT * FROM `shipments` WHERE `id`='$shipping_id'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			return = $row["tracking_id"];
+		}
+	} else {
+		return "None";
+	}
+	$conn->close();
 }
 
 function rackspace_available() {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $sql = "SELECT SUM(rack_size) AS rack_size_sum FROM `rackspace`";
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-          $rack_size_sum = $row["rack_size_sum"];
-      }
-  } else {
-      $rack_size_sum = "0";
-  }
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$sql = "SELECT SUM(rack_size) AS rack_size_sum FROM `rackspace`";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$rack_size_sum = $row["rack_size_sum"];
+		}
+	} else {
+		$rack_size_sum = "0";
+	}
 
-  $sql = "SELECT * FROM `devices` WHERE `device_type`='server' OR `device_type`='router' OR `device_type`='switch' OR `device_type`='PDU'";
-  $result = $conn->query($sql);
-  $rack_used_sum = $result->num_rows;
+	$sql = "SELECT * FROM `devices` WHERE `device_type`='server' OR `device_type`='router' OR `device_type`='switch' OR `device_type`='PDU'";
+	$result = $conn->query($sql);
+	$rack_used_sum = $result->num_rows;
 
-  $rackspace_available = $rack_size_sum - $rack_used_sum;
-  return $rackspace_available;
+	$rackspace_available = $rack_size_sum - $rack_used_sum;
+	return $rackspace_available;
+	$conn->close();
 }
 
 function rackspace_used() {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $sql = "SELECT * FROM `devices` WHERE `device_type`='server' OR `device_type`='router' OR `device_type`='switch' OR `device_type`='PDU'";
-  $result = $conn->query($sql);
-  return $result->num_rows;
-  $conn->close();
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$sql = "SELECT * FROM `devices` WHERE `device_type`='server' OR `device_type`='router' OR `device_type`='switch' OR `device_type`='PDU'";
+	$result = $conn->query($sql);
+	return $result->num_rows;
+	$conn->close();
 }
 
 function hardware_used() {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $sql = "SELECT * FROM `devices` WHERE `device_type`!='server' OR `device_type`!='router' OR `device_type`!='switch' OR `device_type`!='PDU'";
-  $result = $conn->query($sql);
-  return $result->num_rows;
-  $conn->close();
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$sql = "SELECT * FROM `devices` WHERE `device_type`!='server' OR `device_type`!='router' OR `device_type`!='switch' OR `device_type`!='PDU'";
+	$result = $conn->query($sql);
+	return $result->num_rows;
+	$conn->close();
 }
 
 function shipments_inbound() {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $sql = "SELECT * FROM `shipments` WHERE `shipment_status`!='Delivered'";
-  $result = $conn->query($sql);
-  return $result->num_rows;
-  $conn->close();
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$sql = "SELECT * FROM `shipments` WHERE `shipment_status`!='Delivered'";
+	$result = $conn->query($sql);
+	return $result->num_rows;
+	$conn->close();
 }
 function check_if_shipment_in_db($shipment_tracking_id) {
-  include realpath(dirname(__FILE__)).'/../config/db.php';
-  $shipment_tracking_id  = mysqli_real_escape_string($conn, $shipment_tracking_id);
-  $sql = "SELECT * FROM `devices` WHERE `device_tracking_id`='$shipment_tracking_id'";
-  $result = $conn->query($sql);
-  return $result->num_rows;
+	include realpath(dirname(__FILE__)).'/../config/db.php';
+	$shipment_tracking_id  = mysqli_real_escape_string($conn, $shipment_tracking_id);
+	$sql = "SELECT * FROM `devices` WHERE `device_tracking_id`='$shipment_tracking_id'";
+	$result = $conn->query($sql);
+	return $result->num_rows;
+	$conn->close();
 }
 function print_tracking_url($shipment_tracking_id, $shipment_courier) {
-  switch ($shipment_courier) {
-    case 'USPS':
-      return "<a href='https://tools.usps.com/go/TrackConfirmAction.action?tLabels=$shipment_tracking_id' target='_blank'>$shipment_tracking_id</a>";
-      break;
-    
-    case 'FedEX':
-      return "<a href='http://fedex.com/apps/fedextrack/?action=track&trackingnumber=$shipment_tracking_id' target='_blank'>$shipment_tracking_id</a>";
-      break;
+	switch ($shipment_courier) {
+		case 'USPS':
+		return "<a href='https://tools.usps.com/go/TrackConfirmAction.action?tLabels=$shipment_tracking_id' target='_blank'>$shipment_tracking_id</a>";
+		break;
+		
+		case 'FedEX':
+		return "<a href='http://fedex.com/apps/fedextrack/?action=track&trackingnumber=$shipment_tracking_id' target='_blank'>$shipment_tracking_id</a>";
+		break;
 
-    default:
-      return "<a href='https://track.aftership.com/$shipment_tracking_id' target='_blank'>$shipment_tracking_id</a>";
-      break;
-  }
+		default:
+		return "<a href='https://track.aftership.com/$shipment_tracking_id' target='_blank'>$shipment_tracking_id</a>";
+		break;
+	}
 }
 ?>
