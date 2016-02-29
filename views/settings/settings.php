c3296bcdd0a35e6b807184fe2d0b84fc8555ecf6
Arjit Chaudhary <arjit.c@gmail.com>
2016-02-29 22:50:12 +0530

    Fix #61 -- Remove AfterShip API for now

--- views/settings/settings.php
+++ views/settings/settings.php
@@ -3,9 +3,9 @@ $sql = "SELECT * FROM `settings` WHERE `setting`='aftership_api_key'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        $aftership_api_key = $row["value"];
-    }
+  while ($row = $result->fetch_assoc()) {
+    $aftership_api_key = $row["value"];
+  }
 } else {
     //echo "0 results";
 }
@@ -13,9 +13,9 @@ $sql = "SELECT * FROM `settings` WHERE `setting`='librenms_api_key'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        $librenms_api_key = $row["value"];
-    }
+  while ($row = $result->fetch_assoc()) {
+    $librenms_api_key = $row["value"];
+  }
 } else {
     //echo "0 results";
 }
@@ -23,90 +23,71 @@ $sql = "SELECT * FROM `settings` WHERE `setting`='librenms_api_endpoint'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        $librenms_api_endpoint = $row["value"];
-    }
+  while ($row = $result->fetch_assoc()) {
+    $librenms_api_endpoint = $row["value"];
+  }
 } else {
     //echo "0 results";
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DCIMStack</title>
-    <?php
-    include 'libraries/css.php';
-    $token = md5(uniqid(rand(), TRUE));
-    $_SESSION['token'] = $token;
-    ?>
-  </head>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DCIMStack</title>
+  <?php
+  include 'libraries/css.php';
+  $token = md5(uniqid(rand(), TRUE));
+  $_SESSION['token'] = $token;
+  ?>
+</head>
 
-  <body>
+<body>
 
-    <?php include 'libraries/header.php'; ?>
+  <?php include 'libraries/header.php'; ?>
 
-    <div class="container">
-          <h1 class="page-header">DCIMStack Settings</h1>
-          <?php include 'libraries/alerts.php'; ?>
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title"><img src='assets/img/lorry.png'> AfterShip API Key</h3>
-            </div>
-            <div class="panel-body">
-              <p>The AfterShip API is used in the "Shipping" feature of DCIMStack for listing tracking status of your shipments to your datacenter.</p>
-              <p>Current AfterShip API Key: <?php echo $aftership_api_key; ?></p>
-              <form action="settings_db.php" id="aftership_api_key" method="post">
-                <input type="hidden" name="token" value="<?php echo $token; ?>">
-                <input type="hidden" name="setting" value="aftership_api_key">
-                <input type="text" name="value" placeholder="AfterShip API Key" class="form-control" required>
-            </div>
-            <div class="panel-footer">
-              <center><input type="submit" form="aftership_api_key" value="Update" class="btn btn-primary"></center>
-            </div>
-            </form>
-          </div>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title"><img src='assets/img/lorry.png'> LibreNMS API Key</h3>
-            </div>
-            <div class="panel-body">
-              <p>Current LibreNMS API Key: <?php echo $librenms_api_key; ?></p>
-              <form action="settings_db.php" id="librenms_api_key" method="post">
-                <input type="hidden" name="token" value="<?php echo $token; ?>">
-                <input type="hidden" name="setting" value="librenms_api_key">
-                <input type="text" name="value" placeholder="LibreNMS API Key" class="form-control" required>
-            </div>
-            <div class="panel-footer">
-              <center><input type="submit" form="librenms_api_key" value="Update" class="btn btn-primary"></center>
-            </div>
-            </form>
-          </div>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title"><img src='assets/img/lorry.png'> LibreNMS API Endpoint</h3>
-            </div>
-            <div class="panel-body">
-              <p>Example: http://librenms.domain.com/api/v0/devices/</p>
-              <p>Current LibreNMS API Endpoint: <?php echo $librenms_api_endpoint; ?></p>
-              <form action="settings_db.php" id="librenms_api_endpoint" method="post">
-                <input type="hidden" name="token" value="<?php echo $token; ?>">
-                <input type="hidden" name="setting" value="librenms_api_endpoint">
-                <input type="text" name="value" placeholder="LibreNMS API Endpoint" class="form-control" required>
-            </div>
-            <div class="panel-footer">
-              <center><input type="submit" form="librenms_api_endpoint" value="Update" class="btn btn-primary"></center>
-            </div>
-            </form>
-          </div>
+  <div class="container">
+    <h1 class="page-header">DCIMStack Settings</h1>
+    <?php include 'libraries/alerts.php'; ?>
+    <div id="content">
+      <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
+        <li class="active"><a href="#red" data-toggle="tab">Red</a></li>
+        <li><a href="#librenms" data-toggle="tab">LibreNMS</a></li>
+        <li><a href="#yellow" data-toggle="tab">Yellow</a></li>
+        <li><a href="#green" data-toggle="tab">Green</a></li>
+        <li><a href="#blue" data-toggle="tab">Blue</a></li>
+      </ul>
+      <div id="my-tab-content" class="tab-content">
+        <div class="tab-pane active" id="red">
+          <h1>Red</h1>
+          <p>red red red red red red</p>
+        </div>
+        <div class="tab-pane" id="librenms">
+          <h1>LibreNMS</h1>
+          <hr>
+          <?php include 'settings/librenms_tab.php'; ?>
+        </div>
+        <div class="tab-pane" id="yellow">
+          <h1>Yellow</h1>
+          <p>yellow yellow yellow yellow yellow</p>
+        </div>
+        <div class="tab-pane" id="green">
+          <h1>Green</h1>
+          <p>green green green green green</p>
+        </div>
+        <div class="tab-pane" id="blue">
+          <h1>Blue</h1>
+          <p>blue blue blue blue blue</p>
         </div>
       </div>
+    </div>
+  </div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <?php include 'libraries/js.php'; ?>
   </body>
-</html>
+  </html>
