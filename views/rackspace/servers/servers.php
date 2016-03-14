1a830742faf8111acc7e3c99d6a809ed97490172
Arjit Chaudhary <arjit.c@gmail.com>
2016-03-14 19:45:45 +0530

    Update server listing for rackspace

--- views/rackspace/servers/servers.php
+++ views/rackspace/servers/servers.php
@@ -1,38 +1,36 @@
 <?php
-include realpath(dirname(__FILE__)).'/../config/db.php';
-$rackid  = mysqli_real_escape_string($conn, (int)$_GET['rackid']);
+include 'config/db.php';
 $sql = "SELECT * FROM `devices` WHERE `device_type`='server' AND `rackid`='$rackid'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
-  echo "<table class='table' id='search_table'>
-            <thead>
-              <tr>
-                <th>Position</th>
-                <th>Brand</th>
-                <th>Physical Label</th>
-                <th>RAM</th>
-                <th>CPU</th>
-                <th>Size</th>
-                <th>Serial</th>
-              </tr>
-            </thead>
-          <tbody>";
-  	    while($row = $result->fetch_assoc()) {
-  	    	$id = $row["device_id"];
-  	    	echo "<tr>";
-                echo "<td>".$row["device_rack_position"]."</td>";
-  	            echo "<td>".$row["device_brand"]."</td>";
-  	            echo "<td>".$row["device_label"]."</td>";
-  	            echo "<td>".$row["device_ram_total"]."</td>";
-                echo "<td>".device_cpu_count($row["device_cpu"], $row["device_cpu_count"])."</td>";
-  	            echo "<td>".$row["device_size"]."U</td>";
-  	            echo "<td>".$row["device_serial"]."</td>";
-              echo "</tr>";
-  	    }
-  	    echo "</tbody>
-              </table>";
-  	} else {
-  	    echo "<center>No Servers Found</center>";
-  	}     
+  echo "<table class='table' id='search_table'>";
+  echo "<thead>";
+  echo "<tr>";
+  echo "<th>Vendor</th>";
+  echo "<th>Physical Label</th>";
+  echo "<th>Capacity</th>";
+  echo "<th>Serial #</th>";
+  echo "<th>Purchased on</th>";
+  echo "<th>Warranty till</th>";
+  echo "<th><center>Manage</center></th>";
+  echo "</tr>";
+  echo "</thead>";
+  while ($row = $result->fetch_assoc()) {
+    $device_id = $row["device_id"];
+    echo "<tr>";
+    echo "<td>".$row["device_brand"]."</td>";
+    echo "<td>".$row["device_label"]."</td>";
+    echo "<td>".$row["device_capacity"]."</td>";
+    echo "<td>".$row["device_serial"]."</td>";
+    echo "<td>"; if (empty($row["device_dop"])) { echo "0000-00-00"; } else { echo $row["device_dop"]; } echo "</td>";
+    echo "<td>"; if (empty($row["device_warranty"])) { echo "0000-00-00"; } else { echo $row["device_warranty"]; }  echo "</td>";
+    echo "<td><center><a href='manage_server.php?device_id=$device_id' data-remote='false' data-toggle='ajaxModal' data-target='#myModal'>Manage</a></center></td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
