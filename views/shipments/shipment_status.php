4b7f4e319ce8b10938cd9fb50c000485598f85ca
Arjit Chaudhary <arjit.c@gmail.com>
2016-04-12 19:56:20 +0530

    Ability to archive a shipment

--- views/shipments/shipment_status.php
+++ views/shipments/shipment_status.php
@@ -2,6 +2,9 @@
 include 'config/db.php';
 $shipment_id     = mysqli_real_escape_string($conn, $_GET['shipment_id']);
 $shipment_status = mysqli_real_escape_string($conn, $_GET['status']);
+if(empty($shipment_status)) {
+	$shipment_status = mysqli_real_escape_string($conn, $_GET['archive']);
+}
 if(isset($shipment_id, $shipment_status) && ctype_digit($shipment_id)) {
 	if($shipment_status=="undelivered") {
 		$shipment_status = ucfirst($shipment_status);
@@ -13,6 +16,20 @@ if(isset($shipment_id, $shipment_status) && ctype_digit($shipment_id)) {
 	if($shipment_status=="delivered") {
 		$shipment_status = ucfirst($shipment_status);
 		$sql = "UPDATE `shipments` SET `shipment_status`='$shipment_status' WHERE `id`='$shipment_id'";
+		$conn->query($sql);
+		$_SESSION['success'] = "Success, Shipment status updated.";
+		header('Location: shipments.php');
+	}
+	if($shipment_status=="0") {
+		$shipment_status = ucfirst($shipment_status);
+		$sql = "UPDATE `shipments` SET `shipment_archived`='$shipment_status' WHERE `id`='$shipment_id'";
+		$conn->query($sql);
+		$_SESSION['success'] = "Success, Shipment status updated.";
+		header('Location: shipments.php');
+	}
+	if($shipment_status=="1") {
+		$shipment_status = ucfirst($shipment_status);
+		$sql = "UPDATE `shipments` SET `shipment_archived`='$shipment_status' WHERE `id`='$shipment_id'";
 		$conn->query($sql);
 		$_SESSION['success'] = "Success, Shipment status updated.";
 		header('Location: shipments.php');
