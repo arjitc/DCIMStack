26344d6d20945ff1845c751a4a53a3d074bca7df
Arjit Chaudhary <arjit.c@gmail.com>
2016-04-12 22:54:10 +0530

    HipChat API connects

--- views/shipments/shipment_status.php
+++ views/shipments/shipment_status.php
@@ -1,16 +1,20 @@
 <?php
 include 'config/db.php';
+include 'libraries/hipchat.php';
+include 'libraries/general.php';
 $shipment_id     = mysqli_real_escape_string($conn, $_GET['shipment_id']);
 $shipment_status = mysqli_real_escape_string($conn, $_GET['status']);
 if(empty($shipment_status)) {
 	$shipment_status = mysqli_real_escape_string($conn, $_GET['archive']);
 }
+$shipment_tracking_id = get_tracking_from_id($shipment_id);
 if(isset($shipment_id, $shipment_status) && ctype_digit($shipment_id)) {
 	if($shipment_status=="undelivered") {
 		$shipment_status = ucfirst($shipment_status);
 		$sql = "UPDATE `shipments` SET `shipment_status`='$shipment_status' WHERE `id`='$shipment_id'";
 		$conn->query($sql);
 		$_SESSION['success'] = "Success, Shipment status updated.";
+		hipchat_alert("Shipment $shipment_tracking_id has been marked as $shipment_status");
 		header('Location: shipments.php');
 	}
 	if($shipment_status=="delivered") {
@@ -18,6 +22,7 @@ if(isset($shipment_id, $shipment_status) && ctype_digit($shipment_id)) {
 		$sql = "UPDATE `shipments` SET `shipment_status`='$shipment_status' WHERE `id`='$shipment_id'";
 		$conn->query($sql);
 		$_SESSION['success'] = "Success, Shipment status updated.";
+		hipchat_alert("Shipment $shipment_tracking_id has been marked as $shipment_status");
 		header('Location: shipments.php');
 	}
 	if($shipment_status=="0") {
@@ -25,6 +30,7 @@ if(isset($shipment_id, $shipment_status) && ctype_digit($shipment_id)) {
 		$sql = "UPDATE `shipments` SET `shipment_archived`='$shipment_status' WHERE `id`='$shipment_id'";
 		$conn->query($sql);
 		$_SESSION['success'] = "Success, Shipment status updated.";
+		hipchat_alert("Shipment $shipment_tracking_id has been un-archived");
 		header('Location: shipments.php');
 	}
 	if($shipment_status=="1") {
@@ -32,6 +38,7 @@ if(isset($shipment_id, $shipment_status) && ctype_digit($shipment_id)) {
 		$sql = "UPDATE `shipments` SET `shipment_archived`='$shipment_status' WHERE `id`='$shipment_id'";
 		$conn->query($sql);
 		$_SESSION['success'] = "Success, Shipment status updated.";
+		hipchat_alert("Shipment $shipment_tracking_id has been archived");
 		header('Location: shipments.php');
 	}
 } else {
