8bda7ac037e8e6c18b41887004d5756240a5e001
Arjit Chaudhary <arjit.c@gmail.com>
2016-04-12 22:46:06 +0530

    HipChat API connect for shipments

--- views/shipments/update_shipment_db.php
+++ views/shipments/update_shipment_db.php
@@ -1,5 +1,6 @@
 <?php
 include 'config/db.php';
+include 'libraries/hipchat.php';
 $shipment_id = mysqli_real_escape_string($conn, $_POST['shipment_id']);
 $shipment_tracking_id =  mysqli_real_escape_string($conn, $_POST['shipment_tracking_id']);
 $shipment_notes = mysqli_real_escape_string($conn, $_POST['shipment_notes']);
@@ -19,11 +20,13 @@ if(isset($_POST['shipment_notes'], $_POST['shipment_id'])) {
 if(isset($_POST['shipment_status'], $_POST['shipment_id'])) {
     $sql = "UPDATE `shipments` SET `shipment_status`='$shipment_status' WHERE `id`='$shipment_id'";
     $conn->query($sql);
+    hipchat_alert("Shipment $shipment_tracking_id has been updated to $shipment_status");
     $_SESSION['success'] = "Success, Shipment updated.";
 }
 if(isset($_POST['shipment_delivery_eta'], $_POST['shipment_id'])) {
     $sql = "UPDATE `shipments` SET `shipment_delivery_eta`='$shipment_delivery_eta' WHERE `id`='$shipment_id'";
     $conn->query($sql);
+    hipchat_alert("Shipment $shipment_tracking_id delivery ETA has been updated to $shipment_delivery_eta");
     $_SESSION['success'] = "Success, Shipment updated.";
 }
 if(isset($_POST['shipment_courier'], $_POST['shipment_id'])) {
