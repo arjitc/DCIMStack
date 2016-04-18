5096a25ca1afd47f64c7b2f9e5f7f0aaf68fb082
Arjit Chaudhary <arjit.c@gmail.com>
2016-04-18 12:30:46 +0530

    Update rackspace->servers listing table

--- views/rackspace/servers/servers.php
+++ views/rackspace/servers/servers.php
@@ -7,9 +7,12 @@ if ($result->num_rows > 0) {
   echo "<table class='table' id='search_table'>";
   echo "<thead>";
   echo "<tr>";
+  echo "<th>Location</th>";
   echo "<th>Vendor</th>";
   echo "<th>Physical Label</th>";
-  echo "<th>Capacity</th>";
+  echo "<th>MAC Address</th>";
+  echo "<th>MGMT IP</th>";
+  echo "<th>MGMT MAC</th>";
   echo "<th>Serial #</th>";
   echo "<th>Purchased on</th>";
   echo "<th>Warranty till</th>";
@@ -19,9 +22,12 @@ if ($result->num_rows > 0) {
   while ($row = $result->fetch_assoc()) {
     $device_id = $row["device_id"];
     echo "<tr>";
+    echo "<td>".get_rack_name($row['rackid'])."</td>";
     echo "<td>".$row["device_brand"]."</td>";
     echo "<td>".$row["device_label"]."</td>";
-    echo "<td>".$row["device_capacity"]."</td>";
+    echo "<td>".$row["device_mac"]."</td>";
+    echo "<td>".$row["device_mgmt_ip"]."</td>";
+    echo "<td>".$row["device_mgmt_mac"]."</td>";
     echo "<td>".$row["device_serial"]."</td>";
     echo "<td>"; if (empty($row["device_dop"])) { echo "0000-00-00"; } else { echo $row["device_dop"]; } echo "</td>";
     echo "<td>"; if (empty($row["device_warranty"])) { echo "0000-00-00"; } else { echo $row["device_warranty"]; }  echo "</td>";
