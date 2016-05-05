459f1263c9694d0955175620b91622bd6fdc7ac7
Arjit Chaudhary <arjit.c@gmail.com>
2016-05-05 19:00:58 +0530

    Revert "Update servers table listing under rackspace"
    This reverts commit d075a7cfc42842eba04654c22ee0ec7da19692f9.

--- views/rackspace/servers/servers.php
+++ views/rackspace/servers/servers.php
@@ -1,19 +1,21 @@
 <?php
 include 'config/db.php';
-$sql = "SELECT * FROM `devices` WHERE `device_type`='server'";
+$sql = "SELECT * FROM `devices` WHERE `device_type`='server' AND `rackid`='$rackid'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-        // output data of each row
+  // output data of each row
   echo "<table class='table' id='search_table'>";
   echo "<thead>";
   echo "<tr>";
   echo "<th>Location</th>";
   echo "<th>Vendor</th>";
   echo "<th>Physical Label</th>";
-  echo "<th>Customer</th>";
-  echo "<th>IP Address</th>";
   echo "<th>MAC Address</th>";
+  echo "<th>MGMT IP</th>";
+  echo "<th>MGMT MAC</th>";
   echo "<th>Serial #</th>";
+  echo "<th>Purchased on</th>";
+  echo "<th>Warranty till</th>";
   echo "<th><center>Manage</center></th>";
   echo "</tr>";
   echo "</thead>";
@@ -23,11 +25,13 @@ if ($result->num_rows > 0) {
     echo "<td>".get_rack_name($row['rackid'])."</td>";
     echo "<td>".$row["device_brand"]."</td>";
     echo "<td>".$row["device_label"]."</td>";
-    echo "<td>".$row["device_customer"]."</td>";
-    echo "<td>".$row["device_ipaddress"]."</td>";
     echo "<td>".$row["device_mac"]."</td>";
+    echo "<td>".$row["device_mgmt_ip"]."</td>";
+    echo "<td>".$row["device_mgmt_mac"]."</td>";
     echo "<td>".$row["device_serial"]."</td>";
-    echo "<td><center><a href='manage_server.php?device_id=$device_id'>Manage</a></center></td>";
+    echo "<td>"; if (empty($row["device_dop"])) { echo "0000-00-00"; } else { echo $row["device_dop"]; } echo "</td>";
+    echo "<td>"; if (empty($row["device_warranty"])) { echo "0000-00-00"; } else { echo $row["device_warranty"]; }  echo "</td>";
+    echo "<td><center><a href='manage_server.php?device_id=$device_id' data-remote='false' data-toggle='ajaxModal' data-target='#myModal'>Manage</a></center></td>";
     echo "</tr>";
   }
   echo "</table>";
