a2ae16a89f40d28ca41500674066529643592c1f
Arjit Chaudhary <arjit.c@gmail.com>
2016-05-05 12:15:55 +0530

    Allow user to setup IP address

--- views/hardware/manage_server/tab_network.php
+++ views/hardware/manage_server/tab_network.php
@@ -1,10 +1,8 @@
 <br>
-<form action="update_device_db.php" id="ipmi_mgmt" method="post">
+<form action="update_device_db.php" id="network" method="post">
     <input type="hidden" class="form-control" name="device_id" value="<?php echo $device_id; ?>">
-    <label>IPMI/MGMT IP</label>
-    <input type="text" class="form-control" value="<?php echo $device_mgmt_ip; ?>" name="device_mgmt_ip">
-    <label>IPMI/MGMT MAC</label>
-    <input type="text" class="form-control" value="<?php echo $device_mgmt_mac; ?>" name="device_mgmt_mac">
+    <label>IP ADDRESS</label>
+    <input type="text" class="form-control" value="<?php echo $device_ipaddress; ?>" name="device_ipaddress">
     <hr>
-    <center><input type="submit" form="ipmi_mgmt" class="btn btn-primary"></center>
+    <center><input type="submit" form="network" class="btn btn-primary"></center>
 </form>
