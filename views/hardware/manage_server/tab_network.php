77373a04195bb0ea8e5a0ec347d929ab222493ba
Arjit Chaudhary <arjit.c@gmail.com>
2016-05-05 14:44:24 +0530

    Fix conflicting form and tab IDs

--- views/hardware/manage_server/tab_network.php
+++ views/hardware/manage_server/tab_network.php
@@ -1,8 +1,8 @@
 <br>
-<form action="update_device_db.php" id="network" method="post">
+<form action="update_device_db.php" id="device_network" method="post">
     <input type="hidden" class="form-control" name="device_id" value="<?php echo $device_id; ?>">
     <label>IP ADDRESS</label>
     <input type="text" class="form-control" value="<?php echo $device_ipaddress; ?>" name="device_ipaddress">
     <hr>
-    <center><input type="submit" form="network" class="btn btn-primary"></center>
+    <center><input type="submit" form="device_network" class="btn btn-primary"></center>
 </form>
