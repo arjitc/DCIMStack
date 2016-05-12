eb2cfbf38a0487ca4a8276549a3004fc47e883da
Arjit Chaudhary <arjit.c@gmail.com>
2016-05-12 19:22:48 +0530

    Cleanup table and unrequired SQL queries

--- views/hardware/hdd_stats.php
+++ views/hardware/hdd_stats.php
@@ -1,45 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>DCIMStack</title>
-  <?php
-  include 'libraries/css.php';
-  include 'libraries/general.php';
-  ?>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>DCIMStack</title>
+	<?php
+	include 'libraries/css.php';
+	include 'libraries/general.php';
+	?>
 </head>
 
 <body>
 
-  <?php include 'libraries/header.php'; ?>
+	<?php include 'libraries/header.php'; ?>
 
-  <div class="container">
-    <h1 class="page-header">HDD Stats</h1>
-    <?php include 'libraries/alerts.php'; ?>
-    <?php
-    include 'config/db.php';
-    //$sql = "SELECT `device_brand`,COUNT(*) as count FROM `devices` WHERE `device_type` in ('SSD','HDD','SAS') GROUP BY `device_type` ORDER BY count DESC;";
-    $sql = "SELECT `device_brand`, count(*) as `count` from `devices` WHERE `device_type` in ('SSD','HDD','SAS') group by `device_brand`";
-    //$sql = "SELECT `device_brand`,COUNT(*) as count FROM `devices` ";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while ($row = $result->fetch_assoc()) {
-        echo $row["device_brand"];
-        echo $row["count"];
-        echo "<br>";
-      }
-    } else {
-      echo "0 results";
-    }
-    $conn->close();
-    ?>
-  </div>
+	<div class="container">
+		<h1 class="page-header">HDD Stats</h1>
+		<?php include 'libraries/alerts.php'; ?>
+		<?php
+		include 'config/db.php';
+		$sql = "SELECT `device_brand`, count(*) as `count` from `devices` WHERE `device_type` in ('SSD','HDD','SAS') group by `device_brand`";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+      	// output data of each row
+			echo "<table class='table table-hover'>";
+			echo "<tr>";
+				echo "<th>Brand</th>";
+				echo "<th>Drive Count</th>";
+			echo "</tr>";
+			while ($row = $result->fetch_assoc()) {
+				echo "<tr>";
+					echo "<td>".$row["device_brand"]."</td>";
+					echo "<td>".$row["count"]."</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "0 results";
+		}
+		$conn->close();
+		?>
+	</div>
             <!-- Bootstrap core JavaScript
             ================================================== -->
             <!-- Placed at the end of the document so the pages load faster -->
             <?php include 'libraries/js.php'; ?>
-          </body>
-          </html>
+        </body>
+        </html>
