15af5028c5c142556993700390672eda8976bb87
Arjit Chaudhary <arjit.c@gmail.com>
2016-05-19 16:37:13 +0530

    Fix #69 Ability to clear all events

--- libraries/events.php
+++ libraries/events.php
@@ -20,35 +20,38 @@ function list_events_table($limit) {
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
       // output data of each row
-      echo "<div class='table-responsive'>
-            <table class='table' id='search_table'>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Type</th>
-                  <th>Message</th>
-                  <th>Status</th>
-                  <th>Timestamp</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>";
-      while($row = $result->fetch_assoc()) {
-        $id = $row["id"];
-        echo "<tr>";
-              echo "<td>".$row["id"]."</td>";
-              echo "<td>".$row["event_type"]."</td>";
-              echo "<td>".$row["event_message"]."</td>";
-              echo "<td>".$row["event_status"]."</td>";
-              echo "<td>".$row["event_timestamp"]."</td>";
-              echo "<td><a href='delete_event.php?id=$id&token=$token' class='confirmation'><i class='fa fa-times'></i></a></td>";
-            echo "</tr>";
-      }
-      echo "</tbody>
-            </table>
-          </div>";
+    echo "<div class='table-responsive'>
+    <table class='table' id='search_table'>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Type</th>
+          <th>Message</th>
+          <th>Status</th>
+          <th>Timestamp</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>";
+        while($row = $result->fetch_assoc()) {
+          $id = $row["id"];
+          echo "<tr>";
+          echo "<td>".$row["id"]."</td>";
+          echo "<td>".$row["event_type"]."</td>";
+          echo "<td>".$row["event_message"]."</td>";
+          echo "<td>".$row["event_status"]."</td>";
+          echo "<td>".$row["event_timestamp"]."</td>";
+          echo "<td><a href='delete_event.php?id=$id&token=$token' class='confirmation'><i class='fa fa-times'></i></a></td>";
+          echo "</tr>";
+        }
+        echo "</tbody>
+      </table>
+      <div class='pull-right'>
+        <a class='btn btn-primary' href='delete_event.php?id=all&token=$token'>Delete All</a>
+      </div>
+    </div>";
   } else {
-      echo "<center>No events found</center>";
+    echo "<center>No events found</center>";
   }     
 }
 ?>
--- views/events/events.php
+++ views/events/events.php
@@ -1,30 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DCIMStack</title>
-    <?php 
-        include 'libraries/css.php'; 
-        include_once 'libraries/events.php';
-    ?>
-  </head>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DCIMStack</title>
+  <?php 
+  include 'libraries/css.php'; 
+  include_once 'libraries/events.php';
+  ?>
+</head>
 
-  <body>
+<body>
 
-    <?php include 'libraries/header.php'; ?>
+  <?php include 'libraries/header.php'; ?>
+  <div class="container">
+    <h1 class="page-header">Events</h1>
 
-    <div class="container">
-          <h1 class="page-header">Events</h1>
-          
-          <?php list_events_table(0); //from libraries/events.php ?>
-        
-    </div>
+    <?php list_events_table(0); //from libraries/events.php ?>
+    
+  </div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <?php include 'libraries/js.php'; ?>
   </body>
-</html>
+  </html>
