99cdeaa5134eb95fce78ccc8868bca89aeab6873
Arjit Chaudhary <arjit.c@gmail.com>
2016-05-21 15:17:23 +0530

    Display message if server has been assigned to a customer already

--- views/hardware/manage_server/tab_customer.php
+++ views/hardware/manage_server/tab_customer.php
@@ -1,7 +1,15 @@
 <br>
-<form action="update_device_db.php" id="server_information_form" method="post">
+<form action="update_device_db.php" id="customer_form" method="post">
 <input type="hidden" class="form-control" name="device_id" value="<?php echo $device_id; ?>">
 <label>Device Customer</label>
+<?php
+if($device_customer!=0) {
+	echo "<div class='alert alert-info' role='alert'>A customer has been already assigned to this machine";
+	echo "<p>Customer Name: ". get_customer_name_from_id($device_customer) ."</p>";
+	echo "</div>";
+}
+?>
+<label>Assign/Reassign Customer</label>
 <?php
 include 'config/db.php';
 $sql = "SELECT * FROM `customers`";
@@ -9,9 +17,12 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 	echo "<select class='form-control' name='device_customer'>";
 	// output data of each row
+	echo "<option value='$device_customer'>".get_customer_name_from_id($device_customer)."</option>";
 	while ($row = $result->fetch_assoc()) {
 		$customer_id = $row["id"];
-		echo "<option value='$customer_id'>".$row["customer_name"]."</option>";
+		if($customer_id!=$device_customer) {
+			echo "<option value='$customer_id'>".$row["customer_name"]."</option>";
+		}
 	}
 	echo "</select>";
 } else {
@@ -19,5 +30,5 @@ if ($result->num_rows > 0) {
 }
 ?>
 <br>
-<center><input type="submit" form="server_information_form" class="btn btn-primary"></center>
+<center><input type="submit" form="customer_form" class="btn btn-primary"></center>
 </form>
