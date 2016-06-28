0c554b5887d17caa5fa3de84b4cc1c4cb40dd0ba
Arjit Chaudhary <arjit.c@gmail.com>
2016-06-28 18:00:26 +0530

    Cleanup UI

--- views/hardware/servers.php
+++ views/hardware/servers.php
@@ -59,7 +59,7 @@
 	</div>
 	<!-- Add HDD Modal -->
 	<div id="add_hdd" class="modal fade" role="dialog">
-		<div class="modal-dialog">
+		<div class="modal-dialog" style="width: 1200px">
 
 			<!-- Modal content-->
 			<div class="modal-content">
@@ -69,65 +69,71 @@
 				</div>
 				<div class="modal-body">
 					<form action="add_device_db.php" id="add_hdds" method="post">
-						<input type="hidden" name="page_referrer" value="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-						<input type="hidden" class="form-control" name="device_type" value="server">
-						<label>Server CPU</label>
-						<input type="text" class="form-control" name="device_cpu">
-						<label>CPU Count</label>
-						<select class="form-control" name="device_cpu_count">
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-						</select>
-						<label>RAM</label>
-						<div class="input-group">
-							<input type="text" class="form-control" name="device_ram_total">
-							<div class="input-group-addon">GB</div>
+						<div class="row">
+							<div class="col-md-6">
+								<input type="hidden" name="page_referrer" value="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+								<input type="hidden" class="form-control" name="device_type" value="server">
+								<label>Server CPU</label>
+								<input type="text" class="form-control" name="device_cpu">
+								<label>CPU Count</label>
+								<select class="form-control" name="device_cpu_count">
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+								</select>
+								<label>RAM</label>
+								<div class="input-group">
+									<input type="text" class="form-control" name="device_ram_total">
+									<div class="input-group-addon">GB</div>
+								</div>
+								<label>Server Size</label>
+								<select class="form-control" name="device_size">
+									<option value="1U">1U</option>
+									<option value="2U">2U</option>
+									<option value="3U">3U</option>
+									<option value="4U">4U</option>
+								</select>
+								<label>Server Vendor</label>
+								<select class="form-control" name="device_brand">
+									<option value="HP">HP</option>
+									<option value="Dell">Dell</option>
+									<option value="Supermicro">Supermicro</option>
+									<option value="IBM">IBM</option>
+								</select>
+							</div>
+							<div class="col-md-6">
+								<label>Device Location</label>
+								<?php
+								include 'config/db.php';
+								$sql = "SELECT * FROM `rackspace`";
+								$result = $conn->query($sql);
+								if ($result->num_rows > 0) {
+									echo "<select class='form-control' name='device_location'>";
+                        			// output data of each row
+									while ($row = $result->fetch_assoc()) {
+										$rackid = $row["rackid"];
+										echo "<option value='$rackid'>".get_rack_name($rackid)."</option>";
+									}
+									echo "</select>";
+								} else {
+									echo "0 results";
+								}
+								?>
+								<label>Management/IPMI IP</label>
+								<input type="text" class="form-control" name="device_mgmt_ip">
+								<label>Management/IPMI MAC Address</label>
+								<input type="text" class="form-control" name="device_mgmt_mac">
+								<label>Date Of Purchase</label>
+								<input type="date" class="form-control" name="device_dop" required>
+								<label>Warranty valid til</label>
+								<input type="date" class="form-control" name="device_warranty" required>
+								<label>Physical Label</label>
+								<input type="text" class="form-control" name="device_label" required>
+								<label>Serial #</label>
+								<input type="text" class="form-control" name="device_serial" required>
+							</div>
 						</div>
-						<label>Server Size</label>
-						<select class="form-control" name="device_size">
-							<option value="1U">1U</option>
-							<option value="2U">2U</option>
-							<option value="3U">3U</option>
-							<option value="4U">4U</option>
-						</select>
-						<label>Server Vendor</label>
-						<select class="form-control" name="device_brand">
-							<option value="HP">HP</option>
-							<option value="Dell">Dell</option>
-							<option value="Supermicro">Supermicro</option>
-							<option value="IBM">IBM</option>
-						</select>
-						<label>Device Location</label>
-						<?php
-						include 'config/db.php';
-						$sql = "SELECT * FROM `rackspace`";
-						$result = $conn->query($sql);
-						if ($result->num_rows > 0) {
-							echo "<select class='form-control' name='device_location'>";
-                        // output data of each row
-							while ($row = $result->fetch_assoc()) {
-								$rackid = $row["rackid"];
-								echo "<option value='$rackid'>".get_rack_name($rackid)."</option>";
-							}
-							echo "</select>";
-						} else {
-							echo "0 results";
-						}
-						?>
-						<label>Management/IPMI IP</label>
-						<input type="text" class="form-control" name="device_mgmt_ip">
-						<label>Management/IPMI MAC Address</label>
-						<input type="text" class="form-control" name="device_mgmt_mac">
-						<label>Date Of Purchase</label>
-						<input type="date" class="form-control" name="device_dop" required>
-						<label>Warranty valid til</label>
-						<input type="date" class="form-control" name="device_warranty" required>
-						<label>Physical Label</label>
-						<input type="text" class="form-control" name="device_label" required>
-						<label>Serial #</label>
-						<input type="text" class="form-control" name="device_serial" required>
 					</form>
 				</div>
 				<div class="modal-footer">
