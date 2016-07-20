9f198e53022da527dd68c1800072b5e9c3140d86
Arjit Chaudhary <arjit.c@gmail.com>
2016-07-20 09:04:49 +0530

    Added placeholder

--- views/hardware/manage_server/tab_network.php
+++ views/hardware/manage_server/tab_network.php
@@ -2,7 +2,7 @@
 <form action="update_device_db.php" id="device_network" method="post">
     <input type="hidden" class="form-control" name="device_id" value="<?php echo $device_id; ?>">
     <label>IP ADDRESS</label>
-    <input type="text" class="form-control" value="<?php echo $device_ipaddress; ?>" name="device_ipaddress">
+    <input type="text" class="form-control" value="<?php echo $device_ipaddress; ?>" placeholder="127.0.0.1" name="device_ipaddress">
     <hr>
     <center><input type="submit" form="device_network" class="btn btn-primary"></center>
 </form>
