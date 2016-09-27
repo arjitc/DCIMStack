3c28ac70c89754acb6f18c5dff4f6658a9f9d399
Arjit Chaudhary <arjit.c@gmail.com>
2016-09-27 18:59:17 +0530

    Cleanup

--- views/about.php
+++ views/about.php
@@ -1,38 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DCIMStack</title>
-    <?php 
-        include      'libraries/css.php'; 
-    ?>
-  </head>
-
-  <body>
-
-    <?php include 'libraries/header.php'; ?>
-
-    <div class="container">
-          <h1 class="page-header">About</h1>
-          <p>DCIMStack is a open source "Data Center Inventory Management" web-based application/tool</p>
-
-          <h3>Technologies used</h3>
-          <p>DCIMStack is made possible with the help of the following opensource projects, </p>
-            <ul>
-                <li> Bootstrap 3 
-                <li> Fontawesome
-                <li> PHP (Version on this machine <?php echo phpversion(); ?>)
-                <li> MySQL
-                <li> <a href="http://www.famfamfam.com/lab/icons/silk/" target="_blank">Silk Iconset</a> by Mark James
-            </ul>
-        
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <?php include 'libraries/js.php'; ?>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DCIMStack</title>
+  <?php 
+  include 'libraries/css.php'; 
+  ?>
+</head>
+<body>
+  <?php include 'libraries/header.php'; ?>
+  <div class="container">
+    <h1 class="page-header">About</h1>
+    <p>DCIMStack is a open source "Data Center Inventory Management" web-based application/tool</p>
+    <h3>Technologies used</h3>
+    <p>DCIMStack is made possible with the help of the following opensource projects, </p>
+    <ul>
+      <li> Bootstrap 3 </li>
+      <li> Fontawesome </li>
+      <li> PHP (Version on this machine <?php echo phpversion(); ?>) </li>
+      <li> MySQL </li>
+      <li> <a href="http://www.famfamfam.com/lab/icons/silk/" target="_blank">Silk Iconset</a> by Mark James </li>
+    </ul>
+  </div>
+  <?php include 'libraries/js.php'; ?>
+</body>
 </html>
