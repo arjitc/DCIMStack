251c90d5dbace76990c94f3e0522a2db6626f4a7
Arjit Chaudhary <arjit.c@gmail.com>
2016-10-10 19:46:07 +0530

    Move breadcrumb below

--- views/hardware/manage_server/manage_server.php
+++ views/hardware/manage_server/manage_server.php
@@ -44,12 +44,12 @@
 	<?php include 'libraries/header.php'; ?>
 	
 	<div class="container">
+		<h1 class="page-header"><?php echo $device_label; ?></h1>
 		<ol class="breadcrumb">
 			<li><a href="#"><?php echo get_rack_location($device_rack); ?></a></li>
 			<li><a href="rackspace.php?rackid=<?php echo $device_rack; ?>"><?php echo get_rack_name_from_device_id($device_id); ?></a></li>
 			<li class="active"><?php echo $device_label; ?></li>
 		</ol>
-		<h1 class="page-header"><?php echo $device_label; ?></h1>
 		<?php include 'libraries/alerts.php'; ?>
 		<div id="content">
 			<ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
