2a01506c3b75e72e09dc3e744bc40337f73eef30
Arjit Chaudhary <arjit.c@gmail.com>
2016-10-26 01:51:33 +0530

    Cleanup

--- views/events/delete_event.php
+++ views/events/delete_event.php
@@ -1,34 +1,34 @@
 <?php
 include 'config/db.php';
 if ($_SESSION['token'] == $_GET['token'] && isset($_GET['id'])) {
-  if(is_numeric($id)) {
-    $id = mysqli_real_escape_string($conn, (int)$_GET['id']);
-    $sql = "DELETE FROM `events` WHERE `id`='$id'";
-    if ($conn->query($sql) === TRUE) {
-      $_SESSION['success'] = "Success, Event deleted.";
-      unset($_SESSION['token']);
-      header('Location: index.php');
-    } else {
-      $_SESSION['error'] = "Error, Event not deleted.";
-      unset($_SESSION['token']);
-      header('Location: index.php');
-    }
-  } if($_GET['id']=="all") {
-    $id = mysqli_real_escape_string($conn, (int)$_GET['id']);
-    $sql = "DELETE FROM `events`";
-    if ($conn->query($sql) === TRUE) {
-      $_SESSION['success'] = "Success, Event deleted.";
-      unset($_SESSION['token']);
-      header('Location: index.php');
-    } else {
-      $_SESSION['error'] = "Error, Event not deleted.";
-      unset($_SESSION['token']);
-      header('Location: index.php');
-    }
-  }
+	if(is_numeric($id)) {
+		$id = mysqli_real_escape_string($conn, $_GET['id']);
+		$sql = "DELETE FROM `events` WHERE `id`='$id'";
+		if ($conn->query($sql) === TRUE) {
+			$_SESSION['success'] = "Success, Event deleted.";
+			unset($_SESSION['token']);
+			header('Location: index.php');
+		} else {
+			$_SESSION['error'] = "Error, Event not deleted.";
+			unset($_SESSION['token']);
+			header('Location: index.php');
+		}
+	} if($_GET['id']=="all") {
+		$id = mysqli_real_escape_string($conn, $_GET['id']);
+		$sql = "DELETE FROM `events`";
+		if ($conn->query($sql) === TRUE) {
+			$_SESSION['success'] = "Success, Event deleted.";
+			unset($_SESSION['token']);
+			header('Location: index.php');
+		} else {
+			$_SESSION['error'] = "Error, Event not deleted.";
+			unset($_SESSION['token']);
+			header('Location: index.php');
+		}
+	}
 } else {
-  $_SESSION['error'] = "Error, Token mismatch.";
-  unset($_SESSION['token']);
-  header('Location: index.php');
+	$_SESSION['error'] = "Error, Token mismatch.";
+	unset($_SESSION['token']);
+	header('Location: index.php');
 }
 ?>
