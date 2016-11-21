2def47442eb4363ac091f3fa20874e95e7e190c1
Arjit Chaudhary <arjit.c@gmail.com>
2016-11-21 17:39:53 +0530

    Change CSS/UI
    Signed-off-by: Arjit Chaudhary <arjit.c@gmail.com>

--- libraries/css.php
+++ libraries/css.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/spacelab/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="assets/dashboard.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.10/css/dataTables.bootstrap.min.css" rel="stylesheet">
