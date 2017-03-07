1844bd529f7be734dee7a875894d0c22a424d312
Arjit Chaudhary <arjit.c@gmail.com>
2017-03-08 01:28:16 +0530

    [PATCH] Don't display 0 if no customer is assigned to the server
    Signed-off-by: Arjit Chaudhary <arjit.c@gmail.com>

--- views/hardware/servers.php
+++ views/hardware/servers.php
@@ -43,7 +43,11 @@
 				echo "<td>".get_rack_name($row['rackid'])."</td>";
 				echo "<td>".$row["device_brand"]."</td>";
 				echo "<td>".$row["device_label"]."</td>";
-				echo "<td>".get_customer_name_from_id($row["device_customer"])."</td>";
+				if(empty(get_customer_name_from_id($row["device_customer"]))) {
+					echo "<td></td>";
+				} else {
+					echo "<td>".get_customer_name_from_id($row["device_customer"])."</td>";
+				}
 				echo "<td>".$row["device_ipaddress"]."</td>";
 				echo "<td>".$row["device_mac"]."</td>";
 				echo "<td>".$row["device_serial"]."</td>";
