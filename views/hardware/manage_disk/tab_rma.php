574fc1b2e87f06634ee85613b139a31da2ec304b
Arjit Chaudhary <arjit.c@gmail.com>
2017-03-08 01:19:40 +0530

    [UI] cleanup / fixes
    Signed-off-by: Arjit Chaudhary <arjit.c@gmail.com>

--- views/hardware/manage_disk/tab_rma.php
+++ views/hardware/manage_disk/tab_rma.php
@@ -1,33 +1,31 @@
 <br>
 <form action="update_device_db.php" id="device_rma_form" method="post">
-    <input type="hidden" class="form-control" name="device_id" value="<?php echo $device_id; ?>">
-    <label>RMA</label>
-    <small><i>details and notes on this item</i></small><br>
+	<input type="hidden" class="form-control" name="device_id" value="<?php echo $device_id; ?>">
+	<label>RMA</label>
+	<small><i>details and notes on this item</i></small><br>
 
-<?php
-if($device_rma == 'NO') {
-echo "<font color='green'>This is device is not out for RMA</font>";
-} ELSE {
-echo "<font color='red'>Device marked as been RMA'd</font>";
-echo "<br>";
-echo "Date Sent $device_rma_date";
-}
-?>
-    <br>Set device for RMA? 
-<select name=device_rma>
-  <option value="YES">Yes</option>
-  <option value="NO">No</option>
-</select> 
+	<?php
+	if($device_rma == 'NO') {
+		echo "<div class='alert alert-success' role='alert'>This is device is not out for RMA</div>";
+	} else {
+		echo "<div class='alert alert-info' role='alert'>Device marked as been RMA'd</div>";
+		echo "Date Sent $device_rma_date";
+	}
+	?>
+	<label>Set device for RMA?</label>
+	<select name="device_rma" class="form-control">
+		<option value="YES">Yes</option>
+		<option value="NO">No</option>
+	</select> 
 
-<br>
-Add notes to RMA?<br>
-    <input type="text" class="form-control" name="device_rma_notes" value="<?php echo $device_rma_notes; ?>">
-<?
-if ($device_rma_date == '0000-00-00 00:00:00') {
-$device_rma_date = NOW();
-} ELSE {
-$device_rma_date == '';
-?>
-    <hr>
-    <center><input type="submit" form="device_rma_form" value="Update" class="btn btn-primary"></center>
+	<label>Add notes to RMA?</label>
+	<input type="text" class="form-control" name="device_rma_notes" value="<?php echo $device_rma_notes; ?>">
+	<?
+	if ($device_rma_date == '0000-00-00 00:00:00') {
+		$device_rma_date = NOW();
+	} else {
+		$device_rma_date == '';
+	?>
+	<hr>
+	<center><input type="submit" form="device_rma_form" value="Update" class="btn btn-primary"></center>
 </form>
