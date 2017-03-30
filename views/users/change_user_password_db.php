392fd5b50c804b2fa30bf07d9be88de75c075db5
Arjit Chaudhary <arjit.c@gmail.com>
2017-03-31 03:04:09 +0530

    cleanup, added error return messages
    Signed-off-by: Arjit Chaudhary <arjit.c@gmail.com>

--- views/users/change_user_password_db.php
+++ views/users/change_user_password_db.php
@@ -3,27 +3,34 @@ include 'config/db.php';
 $user_id    = mysqli_real_escape_string($conn, $_POST['user_id']);
 $password1    = mysqli_real_escape_string($conn, $_POST['password1']);
 $password2    = mysqli_real_escape_string($conn, $_POST['password2']);
+$token = $_SESSION['token'];
+unset($_SESSION['token']);
 
 if(!isset($user_id)) {
-echo"userid not set";
+	$_SESSION['error'] = "Error, password not added due to user id not set.";
+	header("Location: users.php");
+	exit();
 }
 
 if(!isset($password1)) {
-echo"password1 not set";
+	$_SESSION['error'] = "Error, password not updated due to password 1 not set.";
+	header("Location: users.php");
+	exit();
 }
 
 if(!isset($password2)) {
-echo"password2 not set";
+	$_SESSION['error'] = "Error, password not updated due to password 2 not set.";
+	header("Location: users.php");
+	exit();
 }
-echo"here";
-if(isset($password1, $password2) && $password1 == $password2 && $_SESSION['token']==$_POST['token']) {
-echo"here";
+
+if(isset($password1, $password2) && $password1 == $password2 && $token == $_POST['token']) {
 	$hash = crypt($password1); //Crypt/hash the password
 	$sql = "UPDATE `users` SET `user_password_hash`='$hash' WHERE `user_id`='$user_id'";
-	unset($_SESSION['token']);
+	
 	if ($conn->query($sql) === TRUE) {
-    	$_SESSION['success'] = "Success, Password updated";
-    	header('Location: users.php');
+		$_SESSION['success'] = "Success, Password updated";
+		header('Location: users.php');
 	} else {
 		$_SESSION['error'] = "Error, Password not updated. Perhaps the passwords entered did not match?";
 		header('Location: users.php');
