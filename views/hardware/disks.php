bb97c767022272bcdc8b36f63daa6cb946ec591c
Arjit Chaudhary <arjit.c@gmail.com>
2018-01-25 19:39:38 +0530

    Add Sandisk, Mediamax, Corsair and Whitelabel disk brands

--- views/hardware/disks.php
+++ views/hardware/disks.php
@@ -1,173 +1,175 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>DCIMStack</title>
-  <?php
-  include_once 'libraries/css.php';
-  include_once 'libraries/general.php';
-  ?>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>DCIMStack</title>
+	<?php
+	include_once 'libraries/css.php';
+	include_once 'libraries/general.php';
+	?>
 </head>
 
 <body>
 
-  <?php include_once 'libraries/header.php'; ?>
+	<?php include_once 'libraries/header.php'; ?>
 
-  <div class="container">
-    <h1 class="page-header">Disks 
-      <div class='pull-right'>
-        <button type="button" class='btn btn-primary' data-toggle="modal" data-target="#add_hdd"><img src='assets/img/add.png'> Add</button>
-        <a class='btn btn-primary' href="hdd_stats.php"><img src='assets/img/chart_bar.png'> Stats</a>
-        <a class='btn btn-primary' href="disks.php?var=HDD&filter=inuse"><img src='assets/img/chart_bar.png'> List HDD in-use</a>
-        <a class='btn btn-primary' href="disks.php?var=SSD&filter=inuse"><img src='assets/img/chart_bar.png'> List SSD in-use</a>
-        <a class='btn btn-primary' href="disks.php?var=SAS&filter=inuse"><img src='assets/img/chart_bar.png'> List SAS in-use</a>
-        <a class='btn btn-primary' href="disks.php"><img src='assets/img/chart_bar.png'> Clear filter</a>
-      </div>
-    </h1>
-    <?php include 'libraries/alerts.php'; ?>
-    <?php
-    include_once 'config/db.php';
-    if(isset($_GET['filter']) && isset($_GET['var'])) {
-    	if($_GET['filter']=="inuse" && $_GET['var']=="HDD") {
-    		$sql = "SELECT * FROM `devices` WHERE `device_type`='HDD' AND `device_inuse`=1";
-    	}
-    	if($_GET['filter']=="inuse" && $_GET['var']=="SSD") {
-    		$sql = "SELECT * FROM `devices` WHERE `device_type`='SSD' AND `device_inuse`=1";
-    	}
-    	if($_GET['filter']=="inuse" && $_GET['var']=="SAS") {
-    		$sql = "SELECT * FROM `devices` WHERE `device_type`='SAS' AND `device_inuse`=1";
-    	}
-    } else {
-    	$sql = "SELECT * FROM `devices` WHERE `device_type` in ('SSD','HDD','SAS')";
-    }
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      // output data of each row
-      echo "<table class='table' id='search_table'>";
-      echo "<thead>";
-      echo "<tr>";
-      echo "<th>Location</th>";
-      echo "<th>Vendor</th>";
-      echo "<th>Type</th>";
-      echo "<th>Physical Label</th>";
-      echo "<th>Capacity</th>";
-      echo "<th>Server</th>";
-      echo "<th>Serial #</th>";
-      echo "<th><center>Manage</center></th>";
-      echo "</tr>";
-      echo "</thead>";
-      while ($row = $result->fetch_assoc()) {
-        $device_id = $row["device_id"];
-	$first_echo = '';
-        if($row["device_parent"]!=0 || $row["device_inuse"]==1) { 
-         	$first_echo = "<tr class='info'>"; 
-        }
+	<div class="container">
+		<h1 class="page-header">Disks 
+			<div class='pull-right'>
+				<button type="button" class='btn btn-primary' data-toggle="modal" data-target="#add_hdd"><img src='assets/img/add.png'> Add</button>
+				<a class='btn btn-primary' href="hdd_stats.php"><img src='assets/img/chart_bar.png'> Stats</a>
+				<a class='btn btn-primary' href="disks.php?var=HDD&filter=inuse"><img src='assets/img/chart_bar.png'> List HDD in-use</a>
+				<a class='btn btn-primary' href="disks.php?var=SSD&filter=inuse"><img src='assets/img/chart_bar.png'> List SSD in-use</a>
+				<a class='btn btn-primary' href="disks.php?var=SAS&filter=inuse"><img src='assets/img/chart_bar.png'> List SAS in-use</a>
+				<a class='btn btn-primary' href="disks.php"><img src='assets/img/chart_bar.png'> Clear filter</a>
+			</div>
+		</h1>
+		<?php include 'libraries/alerts.php'; ?>
+		<?php
+		include_once 'config/db.php';
+		if(isset($_GET['filter']) && isset($_GET['var'])) {
+			if($_GET['filter']=="inuse" && $_GET['var']=="HDD") {
+				$sql = "SELECT * FROM `devices` WHERE `device_type`='HDD' AND `device_inuse`=1";
+			}
+			if($_GET['filter']=="inuse" && $_GET['var']=="SSD") {
+				$sql = "SELECT * FROM `devices` WHERE `device_type`='SSD' AND `device_inuse`=1";
+			}
+			if($_GET['filter']=="inuse" && $_GET['var']=="SAS") {
+				$sql = "SELECT * FROM `devices` WHERE `device_type`='SAS' AND `device_inuse`=1";
+			}
+		} else {
+			$sql = "SELECT * FROM `devices` WHERE `device_type` in ('SSD','HDD','SAS')";
+		}
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+      		// output data of each row
+			echo "<table class='table' id='search_table'>";
+			echo "<thead>";
+			echo "<tr>";
+			echo "<th>Location</th>";
+			echo "<th>Vendor</th>";
+			echo "<th>Type</th>";
+			echo "<th>Physical Label</th>";
+			echo "<th>Capacity</th>";
+			echo "<th>Server</th>";
+			echo "<th>Serial #</th>";
+			echo "<th><center>Manage</center></th>";
+			echo "</tr>";
+			echo "</thead>";
+			while ($row = $result->fetch_assoc()) {
+				$device_id = $row["device_id"];
+				$first_echo = '';
+				if($row["device_parent"]!=0 || $row["device_inuse"]==1) { 
+					$first_echo = "<tr class='info'>"; 
+				}
 
-	if($row["device_failed"]=='YES') {
-	        $first_echo = "<tr class='bg-danger'>"; 
-	}
+				if($row["device_failed"]=='YES') {
+					$first_echo = "<tr class='bg-danger'>"; 
+				}
 
-	if ($first_echo == 'NULL' && $row["device_inuse"]!=1) {
-        	$first_echo = "<tr>";
-	}
+				if ($first_echo == 'NULL' && $row["device_inuse"]!=1) {
+					$first_echo = "<tr>";
+				}
 
-	echo $first_echo;
-        echo "<td>".get_rack_name($row['rackid'])."</td>";
-        echo "<td>".$row["device_brand"]."</td>";
-        echo "<td>".$row["device_type"]."</td>";
-        echo "<td>".$row["device_label"]."</td>";
-        echo "<td>".$row["device_capacity"]."</td>";
-        echo "<td>".get_device_label_from_id($row["device_parent"])."</td>";
-        echo "<td>".$row["device_serial"]."</td>";
-        echo "<td><center><a href='manage_disk.php?device_id=$device_id'>Manage</a></center></td>";
-        echo "</tr>";
-      }
-      echo "</table>";
-    } else {
-      echo "0 results";
-    }
-    $conn->close();
-    ?>
-    <div class="pull-right">
-      <small><i>Blue indicates the drive in use, Red indicates a failed drive</i></small>
-    </div>
-  </div>
-  <!-- Add HDD Modal -->
-  <div id="add_hdd" class="modal fade" role="dialog">
-    <div class="modal-dialog" style="width: 1200px">
+				echo $first_echo;
+				echo "<td>".get_rack_name($row['rackid'])."</td>";
+				echo "<td>".$row["device_brand"]."</td>";
+				echo "<td>".$row["device_type"]."</td>";
+				echo "<td>".$row["device_label"]."</td>";
+				echo "<td>".$row["device_capacity"]."</td>";
+				echo "<td>".get_device_label_from_id($row["device_parent"])."</td>";
+				echo "<td>".$row["device_serial"]."</td>";
+				echo "<td><center><a href='manage_disk.php?device_id=$device_id'>Manage</a></center></td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "0 results";
+		}
+		$conn->close();
+		?>
+		<div class="pull-right">
+			<small><i>Blue indicates the drive in use, Red indicates a failed drive</i></small>
+		</div>
+	</div>
+	<!-- Add HDD Modal -->
+	<div id="add_hdd" class="modal fade" role="dialog">
+		<div class="modal-dialog" style="width: 1200px">
 
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title"><img src="assets/img/drive_add.png"> Add Disk</h4>
-        </div>
-        <div class="modal-body">
-          <form action="add_device_db.php" id="add_hdds" method="post">
-            <div class="row">
-              <div class="col-md-6">
-                <input type="hidden" name="page_referrer" value="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-                <label>Device Type</label>
-                <select class="form-control" name="device_type">
-                  <option value="SSD">SSD</option>
-                  <option value="HDD">HDD</option>
-                  <option value="SAS">SAS</option>
-                </select>
-                <label>Device Vendor</label>
-                <select class="form-control" name="device_brand">
-                  <option value="Hitachi">Hitachi</option>
-                  <option value="Seagate">Seagate</option>
-                  <option value="WD">WD</option>
-                  <option value="Samsung">Samsung</option>
-                  <option value="Toshiba">Toshiba</option>
-                  <option value="HP">HP</option>
-                </select>
-                <label>Device Location</label>
-                <?php
-                include 'config/db.php';
-                $sql = "SELECT * FROM `rackspace`";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                  echo "<select class='form-control' name='device_location'>";
-                  // output data of each row
-                  while ($row = $result->fetch_assoc()) {
-                    $rackid = $row["rackid"];
-                    echo "<option value='$rackid'>".get_rack_name($rackid)."</option>";
-                  }
-                  echo "</select>";
-                } else {
-                  echo "0 results";
-                }
-                ?>
-                <label>Device Date Of Purchase</label>
-                <input type="date" class="form-control" name="device_dop" required>
-              </div>
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title"><img src="assets/img/drive_add.png"> Add Disk</h4>
+				</div>
+				<div class="modal-body">
+					<form action="add_device_db.php" id="add_hdds" method="post">
+						<div class="row">
+							<div class="col-md-6">
+								<input type="hidden" name="page_referrer" value="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+								<label>Device Type</label>
+								<select class="form-control" name="device_type">
+									<option value="SSD">SSD</option>
+									<option value="HDD">HDD</option>
+									<option value="SAS">SAS</option>
+								</select>
+								<label>Device Vendor</label>
+								<select class="form-control" name="device_brand">
+									<option value="Hitachi">Hitachi</option>
+									<option value="Seagate">Seagate</option>
+									<option value="WD">WD</option>
+									<option value="Samsung">Samsung</option>
+									<option value="Toshiba">Toshiba</option>
+									<option value="HP">HP</option>
+									<option value="Sandisk">Sandisk</option>
+									<option value="Mediamax">Mediamax</option>
+									<option value="Corsair">Corsair</option>
+									<option value="Whitelabel">Whitelabel</option>
+								</select>
+								<label>Device Location</label>
+								<?php
+								include 'config/db.php';
+								$sql = "SELECT * FROM `rackspace`";
+								$result = $conn->query($sql);
+								if ($result->num_rows > 0) {
+									echo "<select class='form-control' name='device_location'>";
+                  					// output data of each row
+									while ($row = $result->fetch_assoc()) {
+										$rackid = $row["rackid"];
+										echo "<option value='$rackid'>".get_rack_name($rackid)."</option>";
+									}
+									echo "</select>";
+								} else {
+									echo "0 results";
+								}
+								?>
+								<label>Device Date Of Purchase</label>
+								<input type="date" class="form-control" name="device_dop" required>
+							</div>
 
-              <div class="col-md-6">
-                <label>Warranty valid til</label>
-                <input type="date" class="form-control" name="device_warranty" required>
-                <label>Device Label</label>
-                <input type="text" class="form-control" name="device_label" required>
-                <label>Device Serial</label>
-                <input type="text" class="form-control" name="device_serial" required>
-                <label>Device Capacity</label>
-                <input type="text" class="form-control" name="device_capacity" required><br>
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="modal-footer">
-          <input type="submit" form="add_hdds" class="btn btn-primary">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Bootstrap core JavaScript
-  ================================================== -->
-  <!-- Placed at the end of the document so the pages load faster -->
-  <?php include 'libraries/js.php'; ?>
+							<div class="col-md-6">
+								<label>Warranty valid til</label>
+								<input type="date" class="form-control" name="device_warranty" required>
+								<label>Device Label</label>
+								<input type="text" class="form-control" name="device_label" required>
+								<label>Device Serial</label>
+								<input type="text" class="form-control" name="device_serial" required>
+								<label>Device Capacity</label>
+								<input type="text" class="form-control" name="device_capacity" required><br>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="modal-footer">
+					<input type="submit" form="add_hdds" class="btn btn-primary">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php include 'libraries/js.php'; ?>
 </body>
 </html>
