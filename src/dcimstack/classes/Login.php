a04e826eb59f146b1f9dd3553138c583404af82c
Arjit Chaudhary <arjit.c@gmail.com>
2018-11-06 00:54:03 +0530

    Start a session only if PHP_SESSION_NONE

--- src/dcimstack/classes/Login.php
+++ src/dcimstack/classes/Login.php
@@ -26,7 +26,9 @@ class Login
     public function __construct()
     {
         // create/read session, absolutely necessary
-        session_start();
+        if(session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
 
         // check the possible login actions:
         // if user tried to log out (happen when user clicks logout button)
