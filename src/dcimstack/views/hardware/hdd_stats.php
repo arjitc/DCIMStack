e1ceb5f9b7bf4eaaaadb37ef96fcafe736cb4a28
Arjit Chaudhary <arjit.c@gmail.com>
2019-04-17 20:18:48 +0530

    Cleanup layout

--- src/dcimstack/views/hardware/hdd_stats.php
+++ src/dcimstack/views/hardware/hdd_stats.php
@@ -15,62 +15,69 @@
 
 	<?php include 'libraries/header.php'; ?>
 
-	<div class="container">
+	<div class="container-fluid">
 		<h1 class="page-header">HDD Stats</h1>
-		<h2>Usage by brand</h2>
-		<hr>
-		<?php
-		include 'config/db.php';
-		$sql = "SELECT `device_brand`, count(*) as `count` from `devices` WHERE `device_type` in ('SSD','HDD','SAS') group by `device_brand`";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-      	// output data of each row
-			echo "<table class='table table-hover'>";
-			echo "<tr>";
-				echo "<th>Brand</th>";
-				echo "<th>Drive Count</th>";
-			echo "</tr>";
-			while ($row = $result->fetch_assoc()) {
-				echo "<tr>";
-					echo "<td>".$row["device_brand"]."</td>";
-					echo "<td>".$row["count"]."</td>";
-				echo "</tr>";
-			}
-			echo "</table>";
-		} else {
-			echo "0 results";
-		}
-		$conn->close();
-		?>
-		<h2>Usage by capacity</h2>
-		<hr>
-		<?php
-		include 'config/db.php';
-		$sql = "SELECT `device_capacity`, count(*) as `count` from `devices` WHERE `device_type` in ('SSD','HDD','SAS') group by `device_capacity`";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-      	// output data of each row
-			echo "<table class='table table-hover'>";
-			echo "<tr>";
-				echo "<th>Capacity</th>";
-				echo "<th>Drive Count</th>";
-			echo "</tr>";
-			while ($row = $result->fetch_assoc()) {
-				echo "<tr>";
-					echo "<td>".$row["device_capacity"]."</td>";
-					echo "<td>".$row["count"]."</td>";
-				echo "</tr>";
-			}
-			echo "</table>";
-		} else {
-			echo "0 results";
-		}
-		$conn->close();
-		?>
+		<div class="row">
+			<div class="col-md-6">
+				<h2>Usage by brand</h2>
+				<hr>
+				<?php
+				include 'config/db.php';
+				$sql = "SELECT `device_brand`, count(*) as `count` from `devices` WHERE `device_type` in ('SSD','HDD','SAS') group by `device_brand`";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) { // output data of each row
+					echo "<table class='table table-hover'>";
+					echo "<tr>";
+					echo "<th>Brand</th>";
+					echo "<th>Drive Count</th>";
+					echo "</tr>";
+					while ($row = $result->fetch_assoc()) {
+						echo "<tr>";
+						echo "<td>".$row["device_brand"]."</td>";
+						echo "<td>".$row["count"]."</td>";
+						echo "</tr>";
+					}
+					echo "</table>";
+				} else {
+					echo "0 results";
+				}
+				$conn->close();
+				?>
+			</div>
+			<div class="col-md-6">
+				<h2>Usage by capacity</h2>
+				<hr>
+				<?php
+				include 'config/db.php';
+				$sql = "SELECT `device_capacity`, count(*) as `count` from `devices` WHERE `device_type` in ('SSD','HDD','SAS') GROUP BY `device_capacity`";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) { // output data of each row
+					echo "<table class='table table-hover'>";
+					echo "<tr>";
+					echo "<th>Capacity</th>";
+					echo "<th>Drive Count</th>";
+					echo "</tr>";
+					while ($row = $result->fetch_assoc()) {
+						echo "<tr>";
+						echo "<td>".$row["device_capacity"]."</td>";
+						echo "<td>".$row["count"]."</td>";
+						echo "</tr>";
+					}
+					echo "</table>";
+				} else {
+					echo "0 results";
+				}
+				$conn->close();
+				?>
+			</div>
+		</div>
+
+
+		
 	</div>
     <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <?php include 'libraries/js.php'; ?>
+    	================================================== -->
+    	<!-- Placed at the end of the document so the pages load faster -->
+    	<?php include 'libraries/js.php'; ?>
     </body>
-</html>
+    </html>
