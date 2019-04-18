d825dbb51a0433d2fafa442a7f298dbed9f51743
Arjit Chaudhary <arjit.c@gmail.com>
2019-04-18 20:05:30 +0530

    update link in footer to this github project

--- src/dcimstack/libraries/footer.php
+++ src/dcimstack/libraries/footer.php
@@ -1,3 +1,3 @@
 <hr>
-<div class="pull-left">Powered by <a href="http://dcimstack.com/" target="_blank">DCIMStack</a></div>
+<div class="pull-left">Powered by <a href="https://github.com/arjitc/DCIMStack" target="_blank">DCIMStack</a></div>
 <br><br>
