209c1b9833ac754d2526d139ece0ca1d16fbd4bc
Arjit Chaudhary <arjit.c@gmail.com>
2019-04-22 18:54:03 +0530

    add bootstrap-select libs

--- src/dcimstack/libraries/css2.php
+++ src/dcimstack/libraries/css2.php
@@ -4,3 +4,10 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.3/css/bootstrap-select.min.css">
 <link rel="stylesheet" href="//cdn.rawgit.com/Lukas-W/font-linux/v0.7/assets/font-linux.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.10/css/bootstrap-select.css">
+<style type="text/css">
+	.btn-light {
+		background-color: #fff;
+		border: 1px solid #ced4da;
+	}
+</style>
