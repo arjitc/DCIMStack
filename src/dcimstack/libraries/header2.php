92116ddfd363c58c43307985a59132487a95d730
Arjit Chaudhary <arjit.c@gmail.com>
2019-04-22 17:40:10 +0530

    add back missing icon

--- src/dcimstack/libraries/header2.php
+++ src/dcimstack/libraries/header2.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
-  <a class="navbar-brand" href="index.php">DCIMStack</a>
+  <a class="navbar-brand" href="index.php"><i class="fa fa-database"></i> DCIMStack</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
