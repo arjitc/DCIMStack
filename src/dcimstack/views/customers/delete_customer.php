0b36ada8f969108873a6fa8f829531d390cf1858
Arjit Chaudhary <arjit.c@gmail.com>
2021-05-02 05:40:46 +0530

    remove unneeded referrer variable, update error messages

--- src/dcimstack/views/customers/delete_customer.php
+++ src/dcimstack/views/customers/delete_customer.php
@@ -2,20 +2,32 @@
 include 'config/db.php';
 include 'libraries/events.php';
 include 'libraries/general.php';
+
 $customer_id = mysqli_real_escape_string($conn, $_GET['customer_id']);
+
 if (ctype_digit($customer_id) && !empty($customer_id)) {
-  $sql = "DELETE FROM `customers` WHERE `id`='$customer_id'";
-  if ($conn->query($sql) === TRUE) { //set success message
-    $event_type = "Customer Removed";
-    $event_status = "Complete";
-    add_event($event_type, $event_message, $event_status);
-      $_SESSION['success'] = "Success, Customer Removed.";
-  } else { //set error message
-    $_SESSION['error'] = "Error, Customer not removed.";
-  }
-  //redirect back to where we came from
-  $referrer = $_SESSION['referrer'];
-  unset($_SESSION['referrer']); //clear session var
-  header("Location: customers.php"); //redirect!
-} 
+
+	$sql = "DELETE FROM `customers` WHERE `id`='$customer_id'";
+
+	if ($conn->query($sql) === TRUE) {
+
+		$event_type   = "Customer Removed";
+		$event_status = "Complete";
+		add_event($event_type, $event_message, $event_status);
+		
+		$_SESSION['success'] = "Success, Customer Removed.";
+
+	} else { //return back SQL error message
+
+		$_SESSION['error'] = "[SQL ERROR] Error, Customer not removed.";
+
+	}
+	header("Location: customers.php"); //redirect!
+
+} else {
+
+	$_SESSION['error'] = "Error, Customer not removed. Invalid Customer ID / Customer not found.";
+	header("Location: customers.php");
+
+}
 ?>
