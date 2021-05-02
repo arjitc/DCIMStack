6d0f753310f75858dfc7d44b5ea8ab2f5e4f9856
Arjit Chaudhary <arjit.c@gmail.com>
2021-05-02 05:36:01 +0530

    remove add_customer modal code from here

--- src/dcimstack/views/customers/customer_devices.php
+++ src/dcimstack/views/customers/customer_devices.php
@@ -23,8 +23,7 @@
 		include 'config/db.php';
 		$sql = "SELECT * FROM `devices` WHERE `device_customer`='$id'";
 		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-            // output data of each row
+		if ($result->num_rows > 0) { // output data of each row
 			echo "<table class='table' id='search_table'>";
 			echo "<thead>";
 			echo "<tr>";
@@ -53,31 +52,6 @@
 		?>
 	</div>
 
-	<!-- Add Shipment Modal -->
-	<div id="add_customer" class="modal fade" role="dialog">
-		<div class="modal-dialog">
-
-			<!-- Modal content-->
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h4 class="modal-title"><img src='assets/img/user_suit.png'> Add Customer</h4>
-				</div>
-				<div class="modal-body">
-					<form action="add_customer_db.php" id="add_hdds" method="post">
-						<label>Customer name</label>
-						<input type="text" class="form-control" name="customer_name" placeholder="Customer Name" required>
-						<label>Customer Notes</label>
-						<textarea class="form-control" name="customer_notes"></textarea>
-					</form>
-				</div>
-				<div class="modal-footer">
-					<input type="submit" form="add_hdds" class="btn btn-primary">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
-			</div>
-		</div>
-	</div>
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
