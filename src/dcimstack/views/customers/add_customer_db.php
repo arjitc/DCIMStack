a3f3f9513e728248dfd70f2ef350ba28ae2ea941
Arjit Chaudhary <arjit.c@gmail.com>
2021-05-02 05:35:50 +0530

    check for customer_name not being empty, update error messages being returned

--- src/dcimstack/views/customers/add_customer_db.php
+++ src/dcimstack/views/customers/add_customer_db.php
@@ -2,20 +2,35 @@
 include 'libraries/db.php';
 include 'libraries/events.php';
 include 'libraries/general.php';
-$customer_name = mysqli_real_escape_string($conn, $_POST['customer_name']);
-$customer_link = mysqli_real_escape_string($conn, $_POST['customer_link']);
-$customer_notes = mysqli_real_escape_string($conn, $_POST['customer_notes']);
-$sql = "INSERT INTO `customers` (`id`, `customer_name`, `customer_link`, `customer_notes`, `customer_status`, `added_on`) VALUES (NULL, '$customer_name', '$customer_link', '$customer_notes', '1', CURRENT_TIMESTAMP);";
 
-if ($conn->query($sql) === TRUE) {
-	$event_type = "New Customer added";
-	$event_message = "A new customer $customer_name was added";
-	$event_status = "Complete";
-	add_event($event_type, $event_message, $event_status);
-	$_SESSION['success'] = "Success, $customer_name added.";
-	header("Location: customers.php");
+$customer_name  = mysqli_real_escape_string($conn, $_POST['customer_name']);
+$customer_link  = mysqli_real_escape_string($conn, $_POST['customer_link']);
+$customer_notes = mysqli_real_escape_string($conn, $_POST['customer_notes']);
+
+if(!empty($customer_name)) {
+
+	$sql = "INSERT INTO `customers` (`id`, `customer_name`, `customer_link`, `customer_notes`, `customer_status`, `added_on`) VALUES (NULL, '$customer_name', '$customer_link', '$customer_notes', '1', CURRENT_TIMESTAMP);";
+
+	if ($conn->query($sql) === TRUE) {
+
+		$event_type    = "New Customer added";
+		$event_message = "A new customer $customer_name was added";
+		$event_status  = "Complete";
+		add_event($event_type, $event_message, $event_status);
+
+		$_SESSION['success'] = "Success, $customer_name added.";
+		header("Location: customers.php");
+
+	} else { //possible SQL error here
+
+		$_SESSION['error'] = "[SQL ERROR] Error, $customer_name not added.";
+		header("Location: customers.php");
+
+	}
 } else {
-	$_SESSION['error'] = "Error, $customer_name not added.";
+
+	$_SESSION['error'] = "Error, Customer Name was not set.";
 	header("Location: customers.php");
+
 }
 ?>
