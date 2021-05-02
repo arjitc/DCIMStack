bad244ba05068a4cce750db0e3c4fd3050ef10a4
Arjit Chaudhary <arjit.c@gmail.com>
2021-05-02 05:35:23 +0530

    select only what needs to be returned, remove unneeded else statement

--- src/dcimstack/views/graphs/graph_functions.php
+++ src/dcimstack/views/graphs/graph_functions.php
@@ -1,42 +1,33 @@
 <?php
 function librenms_api_key() {
 	include 'config/db.php';
-	$sql = "SELECT * FROM `settings` WHERE `setting`='librenms_api_key'";
+	$sql = "SELECT `value` FROM `settings` WHERE `setting`='librenms_api_key'";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while ($row = $result->fetch_assoc()) {
+	    while ($row = $result->fetch_assoc()) { // output data of each row
 	        return $row["value"];
 	    }
-	} else {
-	    //echo "0 results";
 	}
 }
 function lirenms_api_endpoint() {
 	include 'config/db.php';
-	$sql = "SELECT * FROM `settings` WHERE `setting`='librenms_api_endpoint'";
+	$sql = "SELECT `value` FROM `settings` WHERE `setting`='librenms_api_endpoint'";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while ($row = $result->fetch_assoc()) {
+	    while ($row = $result->fetch_assoc()) { // output data of each row
 	        return $row["value"];
 	    }
-	} else {
-	    //echo "0 results";
 	}
 }
 function port_name($port_number) {
 	include 'config/db.php';
 	$port_number  = mysqli_real_escape_string($conn, $port_number);
-	$sql = "SELECT * FROM `networking` WHERE `port_number`='$port_number'";
+	$sql = "SELECT `port_name` FROM `networking` WHERE `port_number`='$port_number'";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while ($row = $result->fetch_assoc()) {
+	    while ($row = $result->fetch_assoc()) { // output data of each row
 	        return $row["port_name"];
 	    }
-	} else {
-	    //echo "0 results";
 	}
 }
 ?>
