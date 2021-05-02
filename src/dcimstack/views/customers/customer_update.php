1e5246f1a3432f04e9109ce60c0180f3a8e6c826
Arjit Chaudhary <arjit.c@gmail.com>
2021-05-02 05:40:10 +0530

    cleanup

--- src/dcimstack/views/customers/customer_update.php
+++ src/dcimstack/views/customers/customer_update.php
@@ -1,9 +1,9 @@
 <?php
 include 'config/db.php';
-$customer_id = mysqli_real_escape_string($conn, $_POST['customer_id']);
-$customer_name =  mysqli_real_escape_string($conn, $_POST['customer_name']);
+$customer_id    = mysqli_real_escape_string($conn, $_POST['customer_id']);
+$customer_name  =  mysqli_real_escape_string($conn, $_POST['customer_name']);
 $customer_notes = mysqli_real_escape_string($conn, $_POST['customer_notes']);
-$customer_link = mysqli_real_escape_string($conn, $_POST['customer_link']);
+$customer_link  = mysqli_real_escape_string($conn, $_POST['customer_link']);
 
 if(isset($_POST['customer_name'], $_POST['customer_id'])) {
     $sql = "UPDATE `customers` SET `customer_name`='$customer_name' WHERE `id`='$customer_id'";
