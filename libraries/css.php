9d0fe86e4243702efc7c0fe391eceb96e9f7cf5a
Arjit Chaudhary <arjit.c@gmail.com>
2021-10-16 05:19:14 +0530

    Create css.php

---
+++ libraries/css.php
@@ -0,0 +1,3 @@
+<link href="assets/bootstrap-5.1.1-dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/bootstrap-icons-1.5.0/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/dcimstack.css" rel="stylesheet">
