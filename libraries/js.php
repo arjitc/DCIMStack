44036df278bd60b059a1f0a5f42ee73b933061ae
Arjit Chaudhary <arjit.c@gmail.com>
2021-10-16 05:19:17 +0530

    Create js.php

---
+++ libraries/js.php
@@ -0,0 +1,6 @@
+<script src="assets/bootstrap-5.1.1-dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript">
+    $('.confirmation').on('click', function () {
+        return confirm('Are you sure?');
+    });
+</script>
